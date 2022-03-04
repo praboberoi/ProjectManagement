2375dd7e83c695ac04973fdc307320ef4484d7f2
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-03-04 02:36:57 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/model/H2DefaultUserTrigger.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/model/H2DefaultUserTrigger.java
@@ -25,7 +25,7 @@ public class H2DefaultUserTrigger {
 
                 // Create User Database
                 String sql = "CREATE TRIGGER DefaultUserRole " +
-                        "on USERS  " +
+                        "on Users  " +
                         "AFTER INSERT " +
                         "AS " +
                         "BEGIN " +
