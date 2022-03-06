3594b255c454581b3b38f7c27f6362e2bfac9acb
Yaoce Yang <yya139@uclive.ac.nz>
2022-03-06 15:32:14 +1300

    Display created project on current project UI

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/Project.java
@@ -0,0 +1,45 @@
+package nz.ac.canterbury.seng302.portfolio;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Project {
+    private String name;
+    private String startDate;
+    private String endDate;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/DashboardController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/DashboardController.java
@@ -1,6 +1,7 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
 import io.grpc.StatusRuntimeException;
+import nz.ac.canterbury.seng302.portfolio.Project;
 import nz.ac.canterbury.seng302.portfolio.authentication.CookieUtil;
 import nz.ac.canterbury.seng302.portfolio.service.AuthenticateClientService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthenticateResponse;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,13 +19,16 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class DashboardController {
 
-    @Autowired
-    private AuthenticateClientService authenticateClientService;
-
     @GetMapping("/dashboard")
-    public String project(
-
-    ){
+    public String greetingForm(Model model) {
+        model.addAttribute("newProject", new Project());
         return "dashboard";
     }
+
+    @PostMapping("/dashboard")
+    public String greetingSubmit(@ModelAttribute Project project, Model model) {
+        model.addAttribute("newProject", project);
+        return "dashboard";
+    }
+
 }
