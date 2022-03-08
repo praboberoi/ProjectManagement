38a7f2623bcda49f7dd43b458282d9c42e75ee6b
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-03-08 19:00:03 +1300

    #refactor Move database access to shared package

--- shared/src/main/java/nz/ac/canterbury/seng302/shared/projectDAL/H2UsersRolesTable.java
+++ shared/src/main/java/nz/ac/canterbury/seng302/shared/projectDAL/H2UsersRolesTable.java
@@ -1,41 +1,34 @@
-package nz.ac.canterbury.seng302.identityprovider.model;
+package nz.ac.canterbury.seng302.shared.projectDAL;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 
 public class H2UsersRolesTable {
     // JDBC driver name and database URL, JDBC means Java database connectivity
     static final String JDBC_DRIVER = "org.h2.Driver";
-    static final String DB_URL = "jdbc:h2:~/Team15Database";
 
-    //  Database credentials
-    static final String USER = "sa";
-    static final String PASS = "";
 
-    public H2UsersRolesTable()
+    public static void createTable(Datastore db)
     {
-        try (Connection conn = DriverManager.getConnection(DB_URL,USER,PASS); Statement stmt = conn.createStatement()) {
+        try (Connection conn = db.getNewConnection(); Statement stmt = conn.createStatement()) {
             // Registering JDBC driver
             Class.forName(JDBC_DRIVER);
 
             // Create UserToRole Database
-            String sql = "DROP TABLE IF EXISTS UsersRoles; " +
-                    "CREATE TABLE UsersRoles" +
+            String sql = "CREATE TABLE IF NOT EXISTS UsersRoles" +
                     "(userId varchar(20) not NULL, " +
                     "roleId int DEFAULT 1, " +
                     "FOREIGN KEY (userId) REFERENCES Users(userId), " +
                     "FOREIGN KEY (roleId) REFERENCES Roles(roleId), " +
                     "PRIMARY KEY (userId, roleId))";
             stmt.executeUpdate(sql);
-            stmt.close();
-            conn.close();
-        } catch(SQLException se) {
-            se.printStackTrace();
-        } catch(Exception e) {
-            // Handle errors for Class.forName
-            e.printStackTrace();
+        } catch(SQLException sqlException) {
+            System.out.println(LocalDateTime.now() + sqlException.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println(LocalDateTime.now() + "Class.forName failed.");
         }
     }
 
