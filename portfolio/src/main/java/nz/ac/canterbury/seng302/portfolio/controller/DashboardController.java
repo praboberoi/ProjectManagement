96f76156e5ce0a88bd43d9e0cc01d75ab31e7099
rho66 <rho66@uclive.ac.nz>
2022-03-08 21:46:37 +1300

    Added editing functionality to projects

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/DashboardController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/DashboardController.java
@@ -1,28 +1,21 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
-import io.grpc.StatusRuntimeException;
 import nz.ac.canterbury.seng302.portfolio.entity.Project;
-import nz.ac.canterbury.seng302.portfolio.authentication.CookieUtil;
-import nz.ac.canterbury.seng302.portfolio.service.AuthenticateClientService;
 import nz.ac.canterbury.seng302.portfolio.service.DashboardService;
-import nz.ac.canterbury.seng302.shared.identityprovider.AuthenticateResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
 public class DashboardController {
     @Autowired private DashboardService dashboardService;
 
-    @GetMapping("dashboard")
+    @GetMapping("/dashboard")
     public String showProjectList(Model model) {
         List<Project> listProjects = dashboardService.listAll();
         model.addAttribute("listProjects", listProjects);
@@ -32,12 +25,23 @@ public class DashboardController {
     @GetMapping("/dashboard/newProject")
     public String showNewForm(Model model) {
         model.addAttribute("project", new Project());
+        model.addAttribute("pageTitle", "Add New Project");
         return "project_form";
     }
 
     @PostMapping("/dashboard/saveProject")
-     String saveProject(Project project) {
+    public String saveProject(Project project) {
         dashboardService.saveProject(project);
         return "redirect:/dashboard";
     }
+
+    @GetMapping("/dashboard/editProject/{projectName}")
+    public String showEditForm(@PathVariable("projectName") String projectName, Model model) {
+        Project project = dashboardService.getProject(projectName);
+        model.addAttribute("project", project);
+        model.addAttribute("pageTitle", "Edit Project (Name: " + projectName + ")");
+        return "project_form";
+    }
+
+
 }
--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/DashboardService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/DashboardService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DashboardService {
@@ -18,4 +19,12 @@ public class DashboardService {
     public void saveProject(Project project) {
         projectRepo.save(project);
     }
+    
+    public Project getProject(String projectName) {
+        Optional<Project> result = projectRepo.findById(projectName);
+        return result.get();
+
+
+    }
 }
+
