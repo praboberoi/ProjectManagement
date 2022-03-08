33799f19865ab2b318bc3d07e32031838fbafb76
Yaoce Yang <yya139@uclive.ac.nz>
2022-03-08 17:08:00 +1300

    Display projects

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/DashboardService.java
@@ -0,0 +1,17 @@
+package nz.ac.canterbury.seng302.portfolio.service;
+
+import nz.ac.canterbury.seng302.portfolio.entity.Project;
+import nz.ac.canterbury.seng302.portfolio.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DashboardService {
+    @Autowired private ProjectRepository projectRepo;
+
+    public List<Project> listAll() {
+        return (List<Project>) projectRepo.findAll();
+    }
+}
