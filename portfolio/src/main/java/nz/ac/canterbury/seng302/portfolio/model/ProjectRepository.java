cda9ebb3b2d3b99cbe59d04b92a73084a51690c5
Prableen Oberoi <pob16@uclive.ac.nz>
2022-03-24 17:08:49 +1300

    test for projctrepo implemented

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/ProjectRepository.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/ProjectRepository.java
@@ -9,16 +9,16 @@ import java.util.List;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Integer> {
 
-    public List<Project> findByProjectName(String projectName);
+    public Project findByProjectName(String projectName);
 
     public List<Project> findByProjectNameContaining(String name);
 
-    public List<Project> findByDescription(String description);
+    public Project findByDescription(String description);
 
     public List<Project> findByDescriptionContaining(String description);
 
-    public List<Project> findByStartDate(Date startDate);
+    public Project findByStartDate(Date startDate);
 
-    public List<Project> findByEndDate(Date endDate);
+    public Project findByEndDate(Date endDate);
 
 }
---
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/model/ProjectRepositoryTest.java
@@ -0,0 +1,159 @@
+package nz.ac.canterbury.seng302.portfolio.model;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.parameters.P;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProjectRepositoryTest {
+    @Autowired private ProjectRepository projectRepo;
+
+    @Test
+    public void givenProjectExists_FindByProjectName() {
+        Project project1 = new Project.Builder()
+                .projectName("Project 2020")
+                .description("First Attempt")
+                .startDate(new Date(2020, 3, 12))
+                .endDate(new Date(2021, 1, 10))
+                .build();
+
+        Project project2 = new Project.Builder()
+                .projectName("Project 2021")
+                .description("Second Attempt")
+                .startDate(new Date(2021, 1, 9))
+                .endDate(new Date(2021, 11, 20))
+                .build();
+
+
+        projectRepo.save(project1);
+        projectRepo.save(project2);
+        assertEquals(project1, projectRepo.findByProjectName("Project 2020"));
+        assertNotEquals(project2, projectRepo.findByProjectName("Project 2020"));
+        projectRepo.delete(project1);
+        projectRepo.delete(project2);
+
+    }
+
+    @Test
+    public void givenProjectsExists_FindByProjectNameContaining() {
+        Project project1 = new Project.Builder()
+                .projectName("Project 2020")
+                .description("First Attempt")
+                .startDate(new Date(2020, 3, 12))
+                .endDate(new Date(2021, 1, 10))
+                .build();
+
+        Project project2 = new Project.Builder()
+                .projectName("Project 2021")
+                .description("Second Attempt")
+                .startDate(new Date(2021, 1, 9))
+                .endDate(new Date(2021, 11, 20))
+                .build();
+
+
+        projectRepo.save(project1);
+        projectRepo.save(project2);
+        List<Project> projList= Arrays.asList(project1, project2);
+
+        assertArrayEquals(projList.toArray(), projectRepo.findByProjectNameContaining("Project").toArray());
+
+        projectRepo.delete(project1);
+        projectRepo.delete(project2);
+    }
+
+    @Test
+    public void givenDescriptionExists_findByDescription() {
+        Project project1 = new Project.Builder()
+                .projectName("Project 2020")
+                .description("First Attempt")
+                .startDate(new Date(2020, 3, 12))
+                .endDate(new Date(2021, 1, 10))
+                .build();
+
+        Project project2 = new Project.Builder()
+                .projectName("Project 2021")
+                .description("Second Attempt")
+                .startDate(new Date(2021, 1, 9))
+                .endDate(new Date(2021, 11, 20))
+                .build();
+
+        projectRepo.save(project1);
+
+        assertEquals(project1, projectRepo.findByDescription("First Attempt"));
+        assertNotEquals(project2, projectRepo.findByDescription("Second Attempt"));
+
+        projectRepo.delete(project1);
+    }
+
+    @Test
+    public void findByDescriptionContaining() {
+        Project project1 = new Project.Builder()
+                .projectName("Project 2020")
+                .description("First Attempt")
+                .startDate(new Date(2020, 3, 12))
+                .endDate(new Date(2021, 1, 10))
+                .build();
+
+        Project project2 = new Project.Builder()
+                .projectName("Project 2021")
+                .description("Second Attempt")
+                .startDate(new Date(2021, 1, 9))
+                .endDate(new Date(2021, 11, 20))
+                .build();
+
+
+        projectRepo.save(project1);
+        projectRepo.save(project2);
+
+        List<Project> projList= Arrays.asList(project1, project2);
+
+        assertArrayEquals(projList.toArray(), projectRepo.findByDescriptionContaining("Attempt").toArray());
+
+        projectRepo.delete(project1);
+        projectRepo.delete(project2);
+
+    }
+
+    @Test
+    public void findByStartDate() {
+        Project project1 = new Project.Builder()
+                .projectName("Project 2020")
+                .description("First Attempt")
+                .startDate(new Date(2020, 3, 12))
+                .endDate(new Date(2021, 1, 10))
+                .build();
+
+
+        projectRepo.save(project1);
+        assertEquals(project1, projectRepo.findByStartDate(new Date(2020, 3, 12)));
+        assertNotEquals(project1, projectRepo.findByStartDate(new Date(2021, 1, 9)));
+        projectRepo.delete(project1);
+
+    }
+
+    @Test
+    public void findByEndDate() {
+        Project project1 = new Project.Builder()
+                .projectName("Project 2020")
+                .description("First Attempt")
+                .startDate(new Date(2020, 3, 12))
+                .endDate(new Date(2021, 1, 10))
+                .build();
+
+
+        projectRepo.save(project1);
+        assertEquals(project1, projectRepo.findByEndDate(new Date(2021, 1, 10)));
+        assertNotEquals(project1, projectRepo.findByEndDate(new Date(2021, 1, 9)));
+        projectRepo.delete(project1);
+    }
+}
