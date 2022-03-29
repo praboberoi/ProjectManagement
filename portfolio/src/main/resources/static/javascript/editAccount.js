a5690ee2b84e883c5b22087ea9a75f10431cca12
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-03-30 09:31:40 +1300

    #Fix add front end email validation

--- portfolio/src/main/resources/static/javascript/editAccount.js
+++ portfolio/src/main/resources/static/javascript/editAccount.js
@@ -1,6 +1,7 @@
 var editBtn = document.getElementById('editBtn');
 var editables = document.querySelectorAll('#username, #firstname, #lastname');
 var special = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+var email = /^(?=.{1,64}@)[A-Za-z0-9_-]+(\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*(\.[A-Za-z]{2,})$/;
 var digit = /[0-9]/;
 var upper = /[A-Z]/;
 
@@ -74,8 +75,14 @@ function check_nickname() {
 function check_email() {
     let emailElement = document.getElementById("email");
     let emailErrorElement = document.getElementById("emailError");
-    emailElement.classList.remove("form_error");
-    emailErrorElement.innerText = null;
+    if (!email.test(emailElement.value))
+    {
+        emailElement.classList.add("form_error")
+        emailErrorElement.innerText = "Email must be in the form username@domainName.domain."
+    } else {
+        emailElement.classList.remove("form_error");
+        emailErrorElement.innerText = null;
+    }
 };
 
 function check_pronouns() {
