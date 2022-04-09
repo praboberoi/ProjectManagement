200664c7d2d929b950481717b6fbca4e97516a99
Prableen Oberoi <pob16@uclive.ac.nz>
2022-04-09 13:59:43 +1200

    # Feature implemented project modal and fixed the issues with initial implementation. # Fix found a bug with getAllProjects() in DashboardService and fixed it. # manually tested for normal operation.

--- portfolio/src/main/resources/static/javascript/dashboard.js
+++ portfolio/src/main/resources/static/javascript/dashboard.js
@@ -1,15 +1,6 @@
-const deleteButton = document.querySelector('#delProject');
-
-deleteButton.addEventListener('click', () => {
-    const data =  deleteButton.title.split( ' ');
-    const projectId = data[0];
-    const projectName = data.slice(1).join(' ');
-    const modalForm = document.querySelector('#deleteProjectModalForm');
-    modalForm.action = `/dashboard/deleteProject/${projectId}`;
-
-    const modalBody = document.querySelector('#modalBody');
-    const para = document.createElement("p");
-    const node = document.createTextNode(`Are you sure you want to delete ${projectName}`);
-    para.appendChild(node);
-    modalBody.appendChild(para);
-})
+function updateProjectDetails(projectId, projectName) {
+    console.log(projectId);
+    console.log(projectName);
+    document.getElementById('message').innerText =  `Are you sure you want to delete ${projectName}`;
+    document.getElementById('deleteConfirmed').setAttribute('action', `/dashboard/deleteProject/${projectId}`);
+}
