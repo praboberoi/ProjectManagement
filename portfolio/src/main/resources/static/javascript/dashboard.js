184bd344edf82524be6c99cac0897f881661abe6
Prableen Oberoi <pob16@uclive.ac.nz>
2022-04-11 11:46:01 +1200

    # Feature Modal for sprint created. # Tested Manually for proper operation. #Fix project form's alignment of buttons.

--- portfolio/src/main/resources/static/javascript/dashboard.js
+++ portfolio/src/main/resources/static/javascript/dashboard.js
@@ -1,6 +1,4 @@
 function updateProjectDetails(projectId, projectName) {
-    console.log(projectId);
-    console.log(projectName);
     document.getElementById('message').innerText =  `Are you sure you want to delete ${projectName}`;
-    document.getElementById('deleteConfirmed').setAttribute('action', `/dashboard/deleteProject/${projectId}`);
+    document.getElementById('deleteProject').setAttribute('action', `/dashboard/deleteProject/${projectId}`);
 }
