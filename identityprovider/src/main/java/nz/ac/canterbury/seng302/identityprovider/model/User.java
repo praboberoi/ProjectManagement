fd66904a4bf104858d96601362eb77819e9a409c
Sonia Ra <ycr14@uclive.ac.nz>
2022-04-19 18:25:25 +1200

    #feature working on service class for upload/save image functionality

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/model/User.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/model/User.java
@@ -179,6 +179,7 @@ public class User implements Serializable {
         this.password = password;
         this.salt = salt;
         this.roles.add(UserRole.STUDENT);
+//        this.roles.add(UserRole.TEACHER);
         this.dateCreated = new Timestamp(new Date().getTime());
         // this.dateCreated = Timestamp.newBuilder().setSeconds(time.getEpochSecond()).build();
     }
---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/UserProfilePhotoService.java
@@ -0,0 +1,107 @@
+package nz.ac.canterbury.seng302.portfolio.service;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.compiler.PluginProtos;
+import io.grpc.Metadata;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import nz.ac.canterbury.seng302.shared.identityprovider.*;
+import nz.ac.canterbury.seng302.shared.util.FileUploadStatusResponse;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class UserProfilePhotoService {
+
+    @GrpcClient("identity-provider-grpc-server")
+    private UserAccountServiceGrpc.UserAccountServiceStub userAccountStub;
+
+
+    StreamObserver<UploadUserProfilePhotoRequest> streamObserver = this.userAccountStub.uploadUserProfilePhoto(new FileUploadObserver());
+
+//    public FileUploadStatusResponse getImage(
+//            ProfilePhotoUploadMetadata MetaData,
+//            final ByteString fileContent) throws StatusRuntimeException{
+//        FileUploadStatusResponse response = userAccountStub.uploadUserProfilePhoto(UploadUserProfilePhotoRequest.newBuilder()
+//                .setMetaData(MetaData)
+//                .setFileContent(fileContent)
+//                .build());
+//        return response;
+//    }
+
+    //path of the image
+    Path path = Paths.get("");
+
+    // build metadata in UploadUserProfilePhotoRequest proto
+    public UploadUserProfilePhotoRequest getImage(
+            final ProfilePhotoUploadMetadata MetaData) throws StatusRuntimeException, IOException {
+        UploadUserProfilePhotoRequest response = userAccountStub.uploadUserProfilePhoto(UploadUserProfilePhotoRequest.newBuilder()
+                .setMetaData(MetaData.newBuilderForType()
+                        .setUserId()
+                        .setFileType().build())
+                .build());
+
+        streamObserver.onNext(response);
+//        return response;
+
+        // upload file
+        InputStream inputStream = Files.newInputStream(path);
+        byte[] bytes = new byte[4096];
+        int size;
+        while((size = inputStream.read(bytes)) > 0){
+            UploadUserProfilePhotoRequest uploadImage = UploadUserProfilePhotoRequest.newBuilder()
+                    .setFileContent(ByteString.copyFrom(bytes,0,size))
+                    .build();
+            streamObserver.onNext(uploadImage);
+        }
+        inputStream.close();
+        streamObserver.onCompleted();
+
+    return
+    }
+
+
+
+    private static class FileUploadObserver implements StreamObserver<FileUploadStatusResponse>{
+        @Override
+        public void onNext(FileUploadStatusResponse fileUploadStatusResponse) {
+            System.out.println("File upload status" + fileUploadStatusResponse.getStatus());
+        }
+
+        @Override
+        public void onError(Throwable throwable){
+
+        }
+
+        @Override
+        public void onCompleted(){
+
+        }
+    }
+
+
+
+    public StreamObserver<UploadUserProfilePhotoRequest> getImage(
+            final ProfilePhotoUploadMetadata MetaData,
+            final ByteString fileContent) throws StatusRuntimeException{
+        StreamObserver<UploadUserProfilePhotoRequest> response = userAccountStub.uploadUserProfilePhoto(UploadUserProfilePhotoRequest.newBuilder()
+                .setMetaData(MetaData)
+                .setFileContent(fileContent)
+                .build();
+        return response;
+    }
+
+
+//
+//    public FileUploadStatusResponse getImage(int id) throws StatusRuntimeException {
+//        StreamObserver<UploadUserProfilePhotoRequest> response = userAccountStub.uploadUserProfilePhoto(UploadUserProfilePhotoRequest);
+//        return response;
+//    }
+
+}
