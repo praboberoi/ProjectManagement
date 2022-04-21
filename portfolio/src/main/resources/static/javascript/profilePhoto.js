02270197182614d1f6a835d90dbb2b3c1a689a63
Sonia Ra <ycr14@uclive.ac.nz>
2022-04-22 03:17:19 +1200

    #feature caching the image the user uploads into memory

--- portfolio/src/main/resources/static/javascript/profilePhoto.js
+++ portfolio/src/main/resources/static/javascript/profilePhoto.js
@@ -1,4 +1,36 @@
 var loadFile = function (event) {
     var image = document.getElementById("output");
     image.src = URL.createObjectURL(event.target.files[0]);
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        document.getElementById("output").setAttribute("src", e.target.result);
+
+    }
+
+
+    // reader.addEventListener("load", () => {
+    //
+    //     console.log(reader.result);
+    //     localStorage.setItem("recent", reader.result)
+    // })
+    //
+    reader.readAsDataURL(event.target.files[0]);
+
 };
+
+// document.addEventListener("DOMContentLoaded", () =>{
+//     const recentImageDataUrl = localStorage.getItem("recent");
+//
+//     if(recentImageDataUrl){
+//         document.getElementById("output").setAttribute("src", recentImageDataUrl);
+//
+//     }
+// })
+
+// console.log(loadFile());
+//
+// document.querySelector('#formFile').addEventListener("change", function (){
+//     console.log(this.files);
+//
+// })
