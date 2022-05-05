c59008b08670eabf7ac8e9dd31a61187a732b7dc
Saskia van der Peet <saskia.vanderpeet@clearpoint.co.nz>
2022-05-06 11:46:13 +1200

    #feature reverting back to old way to see how to get the items of the JSON objects we have, but now the objects received are empty?

--- portfolio/src/main/resources/static/javascript/calendar.js
+++ portfolio/src/main/resources/static/javascript/calendar.js
@@ -1,16 +1,12 @@
 const PROJECT_ID = window.location.pathname.split('/').slice(-1)
 
-const calendarEl = document.getElementById('calendar');
-
 function getAllEvents() {
     let httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function (){
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             let responseList = []
             const jsonResponseList = JSON.parse(httpRequest.response)
-            console.log(jsonResponseList)
             for (const sprint in jsonResponseList) {
-                console.log(sprint[0])
                 responseList.push(
                     {
                         id: sprint.sprintId,
@@ -20,13 +16,6 @@ function getAllEvents() {
                     }
                 )
             }
-            console.log(responseList)
-            const calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth',
-                events: responseList
-            });
-            calendar.render();
-
         } else {
             console.log(httpRequest.response)
         }
@@ -36,4 +25,12 @@ function getAllEvents() {
     httpRequest.send()
 }
 
-getAllEvents()
+document.addEventListener('DOMContentLoaded', function() {
+    const calendarEl = document.getElementById('calendar');
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        events: getAllEvents()
+    });
+    calendar.render();
+});
+
