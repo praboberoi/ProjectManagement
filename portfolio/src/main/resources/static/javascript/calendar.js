0d3f3fb78c4d4f020378be96f452dd19c734c495
Saskia van der Peet <sva73@uclive.ac.nz>
2022-05-05 16:02:06 +1200

    #feature added the calendar in the right spot for now, and hard coded an event to see what it looks like.

--- portfolio/src/main/resources/static/javascript/calendar.js
+++ portfolio/src/main/resources/static/javascript/calendar.js
@@ -1,7 +1,19 @@
+
+async function getAllEvents() {
+
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const calendarEl = document.getElementById('calendar');
     const calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
+        initialView: 'dayGridMonth',
+        events: [
+            {
+                id: 'a',
+                title: 'my event',
+                start: '2022-05-01'
+            }
+        ]
     });
     calendar.render();
 });
