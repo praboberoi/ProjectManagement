e4a8c08a8c627ae6b337763295ebaa081a167af2
Saskia van der Peet <sva73@uclive.ac.nz>
2022-05-06 12:13:29 +1200

    #feature Trying to refactor to see if it works.

--- portfolio/src/main/resources/static/javascript/calendar.js
+++ portfolio/src/main/resources/static/javascript/calendar.js
@@ -1,16 +1,40 @@
 const PROJECT_ID = window.location.pathname.split('/').slice(-1)
 
-const calendarEl = document.getElementById('calendar');
-const calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-});
 
 
-function getAllEvents() {
+
+// function getAllEvents() {
+//     let httpRequest = new XMLHttpRequest();
+//     httpRequest.onreadystatechange = function (){
+//         if (httpRequest.readyState === XMLHttpRequest.DONE) {
+//             let responseList = []
+//             const jsonResponseList = JSON.parse(httpRequest.response)
+//             jsonResponseList.forEach((sprint, i) => {
+//                 calendar.addEvent(
+//                     {
+//                         id: sprint.sprintId,
+//                         title: sprint.sprintName,
+//                         start: sprint.startDate,
+//                         end: sprint.endDate
+//                     }
+//                 )
+//             })
+//             calendar.render()
+//             console.log(responseList)
+//         } else {
+//             console.log(httpRequest.response)
+//         }
+//     }
+//
+//     httpRequest.open('GET', '/project/' + PROJECT_ID + '/getAllSprints/')
+//     httpRequest.send()
+// }
+
+document.addEventListener('DOMContentLoaded', function() {
     let httpRequest = new XMLHttpRequest();
+    let responseList = []
     httpRequest.onreadystatechange = function (){
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            let responseList = []
             const jsonResponseList = JSON.parse(httpRequest.response)
             jsonResponseList.forEach((sprint, i) => {
                 responseList.push(
@@ -22,7 +46,7 @@ function getAllEvents() {
                     }
                 )
             })
-            console.log(responseList)
+
         } else {
             console.log(httpRequest.response)
         }
@@ -30,10 +54,14 @@ function getAllEvents() {
 
     httpRequest.open('GET', '/project/' + PROJECT_ID + '/getAllSprints/')
     httpRequest.send()
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-    calendar.addEvent(getAllEvents());
+
+    const calendarEl = document.getElementById('calendar');
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        events: responseList
+    });
+    console.log(responseList)
     calendar.render();
 });
 
