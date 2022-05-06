7c8ae46df27c23b8ee0331bf2c06bc59c7f18262
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-05-06 19:43:57 +1200

    #Document Add documentation for the Project Controller class

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProjectController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProjectController.java
@@ -1,31 +1,32 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
 import nz.ac.canterbury.seng302.portfolio.model.Sprint;
-import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
 import nz.ac.canterbury.seng302.portfolio.service.SprintService;
-import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
-import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+/**
+ * A controller which accepts api calls and directs it to the correct service
+ */
 @Controller
 public class ProjectController {
     @Autowired private SprintService sprintService;
-    @Autowired private ProjectService projectService;
-    @Autowired private UserAccountClientService userAccountClientService;
 
+    /**
+     * Gets all of the sprints and returns it in a ResponseEntity
+     * @param projectId The Id of the project to get sprints from
+     * @return A ResponseEntity containing a list of sprints
+     */
     @GetMapping("/project/{projectId}/getAllSprints")
     public ResponseEntity<List<Sprint>> getAllSprints(
-            @PathVariable("projectId") int projectId,
-            @AuthenticationPrincipal AuthState principal) {
+            @PathVariable("projectId") int projectId) {
         List<Sprint> listSprints = sprintService.getSprintByProject(projectId);
         return ResponseEntity.status(HttpStatus.OK).body(listSprints);
     }
