36a263be02dca3bfca56c40b85712ed2ff4500a3
Saskia van der Peet <sva73@uclive.ac.nz>
2022-05-09 16:46:45 +1200

    #chore testing whether our .yml file updates work.

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,2 +1,4 @@
 fuser -k 9500/tcp || true
-java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar --spring.application.name=identity-provider --grpc.server.port=9500
+java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
+      --spring.application.name=identity-provider \
+      --grpc.server.port=9500
