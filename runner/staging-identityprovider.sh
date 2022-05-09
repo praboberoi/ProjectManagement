a303d258070f8e9e0fbf12cb00946abece484dc1
Saskia van der Peet <sva73@uclive.ac.nz>
2022-05-09 16:55:00 +1200

    #chore create a staging profile.

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,4 +1,5 @@
 fuser -k 9500/tcp || true
 java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
       --spring.application.name=identity-provider \
-      --grpc.server.port=9500
+      --grpc.server.port=9500 \
+      --spring.profiles.active=staging
