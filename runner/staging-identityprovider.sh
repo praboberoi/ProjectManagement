5a308f75d39ada59b385937f29f0dad7185fd54f
Prableen Oberoi <pob16@uclive.ac.nz>
2022-05-10 12:10:45 +1200

    #fix the CI stuff for deployment

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,5 +1,5 @@
 fuser -k 9500/tcp || true
-source /gitlab-runner/staging-identityprovider/env.sh
+source staging-identityprovider/env.sh
 java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
       --spring.application.name=identity-provider \
       --grpc.server.port=9500 \
