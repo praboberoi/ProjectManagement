428cf4ddee439747288cb75990636b00fdd85548
Prableen Oberoi <pob16@uclive.ac.nz>
2022-05-11 16:48:51 +1200

    updated the branch

--- portfolio/src/main/resources/static/javascript/project.js
+++ portfolio/src/main/resources/static/javascript/project.js
@@ -1,4 +1,4 @@
-function updateSprintDetails(sprintId, sprintName, projectId) {
+function updateSprintDetails(sprintId, sprintName, projectId, prefix) {
     document.getElementById('message').innerText =  `Are you sure you want to delete ${sprintName}`;
-    document.getElementById('deleteSprint').setAttribute('action', `${projectId}/deleteSprint/${sprintId}`);
+    document.getElementById('deleteSprint').setAttribute('action', `${prefix}/${projectId}/deleteSprint/${sprintId}`);
 }
