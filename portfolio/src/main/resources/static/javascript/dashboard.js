28e649f8b605204ca3e76a4879cf5bccde0968c8
Prableen Oberoi <pob16@uclive.ac.nz>
2022-05-21 14:23:23 +1200

    #Fix added code to the Js for null values of api prefix. Updated the html pages

--- portfolio/src/main/resources/static/javascript/dashboard.js
+++ portfolio/src/main/resources/static/javascript/dashboard.js
@@ -1,4 +1,6 @@
 function updateProjectDetails(projectId, projectName, apiPrefix) {
+    if(apiPrefix === null)
+        apiPrefix = ""
     document.getElementById('message').innerText =  `Are you sure you want to delete ${projectName}`;
     document.getElementById('deleteProject').setAttribute('action', `${apiPrefix}/dashboard/deleteProject/${projectId}`);
 }
