e1ea9e574c29505fab894583c02cfd9db7b65b40
ycr14 <ycr14@uclive.ac.nz>
2022-06-03 15:03:03 +1200

    #feature For screen sizes below medium made hamburger menu appear and made sidebar hidden

---
+++ portfolio/src/main/resources/static/javascript/sidebar.js
@@ -0,0 +1,35 @@
+// function navToggle() {
+    // document.getElementById('sidenav-2').classList.toggle("open");
+    // document.getElementById('sidenav-2').classList.toggle("")
+// }
+
+// const showSidebar = document.querySelector('.sidebar')
+//
+// showSidebar.onclick = function (){
+//     menu.classlist.toggle("open")
+// }
+
+// const show = document.getElementsByClassName("sidenav")
+//
+// document.getElementsByClassName("btn-primary").onclick = function(){
+//     menu.cl
+// }
+
+
+// const show = document.querySelector("button")
+// const menu = document.getElementsByClassName("sidenav")
+// const mee = document.getElementById("sidenav-1")
+//
+// show.addEventListener("click", toggle)
+
+function navToggle(){
+    document.getElementById('sidebar').style.display='block';
+    console.log("open nav")
+    // const m = document.getElementsByClassName("sidenav").style.display="inline"
+    // menu.classList.toggle("sidebar")
+    // show.classList.toggle("sidebar")
+    // mee.classList.toggle("sidebar")
+
+
+
+}
