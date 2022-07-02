68aab781032d6ff8408abd7596287914f943cc89
Sonia Ra <ycr14@uclive.ac.nz>
2022-07-02 16:13:27 +1200

    #documentaion Added documentation and comments, and cleaned up code. Fixed a minor bug in editAccount page.

--- portfolio/src/main/resources/static/javascript/sidebar.js
+++ portfolio/src/main/resources/static/javascript/sidebar.js
@@ -1,44 +1,11 @@
-// function navToggle() {
-    // document.getElementById('sidenav-2').classList.toggle("open");
-    // document.getElementById('sidenav-2').classList.toggle("")
-// }
-
-// const showSidebar = document.querySelector('.sidebar')
-//
-// showSidebar.onclick = function (){
-//     menu.classlist.toggle("open")
-// }
-
-// const show = document.getElementsByClassName("sidenav")
-//
-// document.getElementsByClassName("btn-primary").onclick = function(){
-//     menu.cl
-// }
-
-
-// const show = document.querySelector("button")
-// const menu = document.getElementsByClassName("sidenav")
-// const mee = document.getElementById("sidenav-1")
-//
-// show.addEventListener("click", toggle)
-
+//Check if the sidebar is open, if it is close the sidebar when the hamburger menu is clicked
+//Otherwise open the sidebar when the hamburger menu is clicked
 function navToggle() {
     var checkSidebar = document.getElementById('sidebar');
     if (checkSidebar.style.display === 'block') {
         checkSidebar.style.display = 'none';
     } else {
-        document.getElementById('sidebar').style.display = 'block';
-        document.getElementById('sidebar').style.width = "175px";
-        console.log("open nav")
+       checkSidebar.style.display = 'block';
+       checkSidebar.style.width = "175px";
     }
-    // const m = document.getElementsByClassName("sidenav").style.display="inline"
-    // menu.classList.toggle("sidebar")
-    // show.classList.toggle("sidebar")
-    // mee.classList.toggle("sidebar")
-
 }
-
-// document.getElementById("hamburger-toggle").onclick = function() {
-//     document.getElementById("sidebar").style.display = "block";
-//     console.log("navbar showup")
-// }
