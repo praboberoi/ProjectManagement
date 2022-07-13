8748315e2029b06041f2e9ce3297458a41ec97c0
Saskia van der Peet <sva73@uclive.ac.nz>
2022-07-14 11:36:27 +1200

    #feature #pair[sva73, pob16] working on the image cropping and compression.

--- portfolio/src/main/resources/static/javascript/profilePhoto.js
+++ portfolio/src/main/resources/static/javascript/profilePhoto.js
@@ -1,13 +1,77 @@
-var loadFile = function (event) {
+// let compressedImageBlob;
+// let imgToCompress;
+
+var loadFile = async function (event) {
     var image = document.getElementById("output");
-    image.src = URL.createObjectURL(event.target.files[0]);
+    image.src = URL.createObjectURL(event.target.files[0]); // the link:
+
+    // imgToCompress = image;
+    //
+    // const imageSize = event.target.files[0].size;
+    // const MAX_IMAGE_SIZE = 1000000;
+    // const compressionCoeff = MAX_IMAGE_SIZE / imageSize;
+    //
+    // await compressImage(image, 0.8, compressionCoeff)
+    // console.log("OK")
+    //
+    // console.log(compressedImageBlob)
+    // console.log(typeof compressedImageBlob)
 
     const reader = new FileReader();
     reader.onload = function (e) {
         document.getElementById("output").setAttribute("src", e.target.result);
-
     }
 
     reader.readAsDataURL(event.target.files[0]);
 
 };
+
+// const compressImage = async (image, resizingFactor, quality) => {
+//     // resizing the image
+//     const canvas = document.createElement("canvas");
+//     const context = canvas.getContext("2d");
+//
+//     const originalWidth = imgToCompress.width;
+//     const originalHeight = imgToCompress.height;
+//
+//     const canvasWidth = originalWidth * resizingFactor;
+//     const canvasHeight = originalHeight * resizingFactor;
+//
+//     canvas.width = canvasWidth;
+//     canvas.height = canvasHeight;
+//
+//     context.drawImage(
+//         imgToCompress,
+//         0,
+//         0,
+//         originalWidth * resizingFactor,
+//         originalHeight * resizingFactor
+//     );
+//
+//     // reducing the quality of the image
+//     canvas.toBlob(
+//         (blob) => {
+//             if (blob) {
+//                 // showing the compressed image
+//                 compressedImageBlob = blob;
+//                 imgToCompress.src = URL.createObjectURL(compressedImageBlob);
+//                 image.innerHTML = bytesToSize(blob.size);
+//             }
+//         },
+//         "image/jpeg",
+//         quality
+//     );
+//
+// }
+//
+// // source: https://stackoverflow.com/a/18650828
+// function bytesToSize(bytes) {
+//     var sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+//
+//     if (bytes === 0) {
+//         return "0 Byte";
+//     }
+//     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+//
+//     return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
+// }
