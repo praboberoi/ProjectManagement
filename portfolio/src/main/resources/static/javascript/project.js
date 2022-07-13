9459f7a3929d66898ac1a062245a14e3c5140ef9
Prableen Oberoi <pob16@uclive.ac.nz>
2022-07-14 00:13:13 +1200

    #feature programmed the tabs on the project page to display tabs for monthly planner and project description

--- portfolio/src/main/resources/static/javascript/project.js
+++ portfolio/src/main/resources/static/javascript/project.js
@@ -1,6 +1,35 @@
+const projectDescriptionNav = document.getElementById('projectDescriptionNav')
+const plannerNav = document.getElementById('plannerNav')
+const calender = document.getElementById('cal')
+const projectDescription = document.getElementById('projectDescription')
+
 function updateSprintDetails(sprintId, sprintName, projectId, prefix) {
     if (prefix === null)
         prefix = ""
     document.getElementById('message').innerText = `Are you sure you want to delete ${sprintName}`;
     document.getElementById('deleteSprint').setAttribute('action', `${prefix}/${projectId}/deleteSprint/${sprintId}`);
+}
+
+function navTOProjectDescription() {
+    if (projectDescriptionNav.ariaSelected === "false") {
+        calender.hidden = true
+        projectDescription.hidden = false
+        projectDescriptionNav.ariaSelected = "true";
+        plannerNav.ariaSelected = "false"
+        plannerNav.classList.remove('active')
+        projectDescriptionNav.classList.add('active')
+
+    }
+}
+
+function navToPlanner() {
+    if (plannerNav.ariaSelected === "false") {
+        calender.hidden = false
+        projectDescription.hidden = true
+        projectDescriptionNav.ariaSelected = "false";
+        plannerNav.ariaSelected = "true"
+        projectDescriptionNav.classList.remove('active')
+        plannerNav.classList.add('active')
+    }
+
 }
