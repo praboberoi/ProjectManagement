38b94b0a88a3037c24551c8e2244af12b0d3850c
Prableen Oberoi <pob16@uclive.ac.nz>
2022-07-16 15:43:07 +1200

    #feature #pair[sva73, pob16] programmed the image cropping and modal. #testManual #pair[sva73, pob16] tested it for proper operation and have to fix the submit section.

--- portfolio/src/main/resources/static/javascript/profilePhoto.js
+++ portfolio/src/main/resources/static/javascript/profilePhoto.js
@@ -1,92 +1,34 @@
-// let compressedImageBlob;
-// let imgToCompress;
+let croppie;
+let image;
+let fileName;
+async function loadFile (event) {
 
-var loadFile = async function (event) {
-    var image = document.getElementById("output");
-    image.src = URL.createObjectURL(event.target.files[0]); // the link:
-    var vanilla = new Croppie(image, {
-        viewport: { width: 100, height: 100 },
+    image = document.getElementById("output")
+    image.src = URL.createObjectURL(event.target.files[0])
+    fileName = event.target.files[0].name;
+    document.getElementById('uploadImage').classList.add('ready');
+
+    if (typeof croppie !== "undefined")
+        croppie.destroy()
+
+    croppie = new Croppie(document.getElementById('uploadImage'),{
+        viewport: { width: 200, height: 200, type: 'circle'},
         boundary: { width: 300, height: 300 },
-        showZoomer: false,
-        enableOrientation: true
+        showZoomer: false
     });
-    vanilla.bind({
-        url: image.src,
-        orientation: 4
-    });
-//on button click
-    vanilla.result('blob').then(function(blob) {
-        // do something with cropped blob
-    });
-    // imgToCompress = image;
-    //
-    // const imageSize = event.target.files[0].size;
-    // const MAX_IMAGE_SIZE = 1000000;
-    // const compressionCoeff = MAX_IMAGE_SIZE / imageSize;
-    //
-    // await compressImage(image, 0.8, compressionCoeff)
-    // console.log("OK")
-    //
-    // console.log(compressedImageBlob)
-    // console.log(typeof compressedImageBlob)
+    croppie.bind({url: image.src})
+}
 
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        document.getElementById("output").setAttribute("src", e.target.result);
-    }
-
-    reader.readAsDataURL(event.target.files[0]);
-
-};
-
-
-// const compressImage = async (image, resizingFactor, quality) => {
-//     // resizing the image
-//     const canvas = document.createElement("canvas");
-//     const context = canvas.getContext("2d");
-//
-//     const originalWidth = imgToCompress.width;
-//     const originalHeight = imgToCompress.height;
-//
-//     const canvasWidth = originalWidth * resizingFactor;
-//     const canvasHeight = originalHeight * resizingFactor;
-//
-//     canvas.width = canvasWidth;
-//     canvas.height = canvasHeight;
-//
-//     context.drawImage(
-//         imgToCompress,
-//         0,
-//         0,
-//         originalWidth * resizingFactor,
-//         originalHeight * resizingFactor
-//     );
-//
-//     // reducing the quality of the image
-//     canvas.toBlob(
-//         (blob) => {
-//             if (blob) {
-//                 // showing the compressed image
-//                 compressedImageBlob = blob;
-//                 imgToCompress.src = URL.createObjectURL(compressedImageBlob);
-//                 image.innerHTML = bytesToSize(blob.size);
-//             }
-//         },
-//         "image/jpeg",
-//         quality
-//     );
-//
-// }
-//
-// // source: https://stackoverflow.com/a/18650828
-// function bytesToSize(bytes) {
-//     var sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-//
-//     if (bytes === 0) {
-//         return "0 Byte";
-//     }
-//     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-//
-//     return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
-// }
+function cropImage() {
+    const imageType = fileName.split(".")[1]
+    croppie.result({type:'blob', format: imageType, circle:'true'})
+        .then(function (imgBlob) {
+            console.log(imgBlob.size)
+            image.src = URL.createObjectURL(imgBlob)
+            const file = new File([imgBlob], fileName,{type:imgBlob.type, lastModified:new Date().getTime()}, 'utf-8');
+            const container = new DataTransfer();
+            container.items.add(file);
+            document.getElementById('formFile').files = container.files;
+        })
+}
 
