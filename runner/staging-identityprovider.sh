aaa9cfcd47a7e62c4c3d11b8a2e47360fa6e6d14
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-07-18 10:43:32 +1200

    #Fix #Chore Update script files to correct ports

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -2,5 +2,6 @@ fuser -k 9500/tcp || true
 source staging-identityprovider/env.sh
 java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
       --spring.application.name=identity-provider \
-      --grpc.server.port=9500 \
+      --grpc.server.port=9502 \
       --spring.profiles.active=staging
+      --server.port=9500
