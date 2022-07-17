d9054518a81a6faa18bb35fdc7ac730bfd62408f
Prableen Oberoi <pob16@uclive.ac.nz>
2022-07-18 11:35:52 +1200

    #chor to fix the develop pipeline

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -3,5 +3,5 @@ source staging-identityprovider/env.sh
 java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
       --spring.application.name=identity-provider \
       --grpc.server.port=9502 \
-      --spring.profiles.active=staging
+      --spring.profiles.active=staging \
       --server.port=9500
