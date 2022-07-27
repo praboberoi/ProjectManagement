10a60ef4df28360d30ac05f5e4ba81afd34d24a0
Yaoce Yang <yya139@uclive.ac.nz>
2022-07-27 16:32:51 +1200

    Update error message, delete print statements, and make code quality changes.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/EventService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/EventService.java
@@ -2,14 +2,12 @@ package nz.ac.canterbury.seng302.portfolio.service;
 
 import nz.ac.canterbury.seng302.portfolio.model.Event;
 import nz.ac.canterbury.seng302.portfolio.model.EventRepository;
-import nz.ac.canterbury.seng302.portfolio.model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 
 /**
     Client service used to communicate to the database
@@ -43,37 +41,27 @@ public class EventService {
     public String verifyEvent(Event event) throws ParseException {
         if (event == null) return ("No Event");
 
-        if (event.getEventName() == null || event.getEndDate() == null || event.getStartDate() == null)
+        if (event.getEventName() == null || event.getEndDate() == null || event.getStartDate() == null || event.getStartTime() == null || event.getEndTime() == null) {
             return ("Event values are null");
-
-        if (event.getEndDate() != null && event.getStartDate() != null) {
-            if (event.getEndDate().before(event.getStartDate())){
-                return ("The event end date cannot be before the event start date");
-            }
-        }
-        if (event.getStartTime() != null && event.getEndTime() != null) {
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        } else if (event.getEndDate().before(event.getStartDate())) {
+            return ("The event end date cannot be before the event start date");
+        } else {
             try {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
                 Date d1 = (Date) sdf.parse(event.getStartTime());
                 Date d2 = (Date) sdf.parse(event.getEndTime());
                 // Check start date is same as end date
-                if (event.getStartDate().equals(event.getEndDate())) {
-                    // End time before start time
-                    if (d2.before(d1)) {
-                        return ("The end time cannot be before the start time when event is on the same day");
-                    }
-                    // Start and end time is the same
-                    if (d2.equals(d1)) {
-                        return ("The end time cannot be the same as the start time when event is on the same day");
-                    }
+                if (event.getStartDate().equals(event.getEndDate()) && (d2.before(d1) || d2.equals(d1))) {
+                    // End time before start time or Start and end time is the same
+                    return ("The event must start before the event ends");
                 }
+                return("Event has been verified");
             } catch (ParseException e) {
                 return ("Error with start and end time");
             } catch (Exception e) {
                 return ("Error with start and end time validation");
             }
         }
-        return("Event has been verified");
     }
 
     /**
@@ -84,13 +72,10 @@ public class EventService {
     public String saveEvent(Event event) throws Exception {
         String message;
         if (isNew) {
-            System.out.println("new event");
-            System.out.println(event.getStartDate());
             currentEvent = event;
             message = "Successfully Created " + event.getEventName();
             isNew = false;
         } else {
-            System.out.println("edit existing event");
             currentEvent.setEventName(event.getEventName());
             currentEvent.setEndDate(event.getEndDate());
             currentEvent.setStartDate(event.getStartDate());
--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/model/EventServiceTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/model/EventServiceTest.java
@@ -41,7 +41,7 @@ public class EventServiceTest {
         Event emptyEvent = null;
         assertNull(emptyEvent);
         String resultString = eventService.verifyEvent(emptyEvent);
-        assertEquals(resultString, "No Event");
+        assertEquals("No Event", resultString);
     }
 
     /**
@@ -52,7 +52,7 @@ public class EventServiceTest {
         Event emptyEvent = new Event();
         assertNull(emptyEvent.getEventName());
         String resultString = eventService.verifyEvent(emptyEvent);
-        assertEquals(resultString, "Event values are null");
+        assertEquals("Event values are null", resultString);
     }
 
     /**
@@ -69,7 +69,7 @@ public class EventServiceTest {
                 .endTime("21:00")
                 .build();
         String resultString = eventService.verifyEvent(newEvent);
-        assertEquals(resultString, "The event end date cannot be before the event start date");
+        assertEquals("The event end date cannot be before the event start date", resultString);
     }
 
     /**
@@ -86,7 +86,7 @@ public class EventServiceTest {
                 .endTime("20:00")
                 .build();
         String resultString = eventService.verifyEvent(newEvent);
-        assertEquals(resultString, "The end time cannot be the same as the start time when event is on the same day");
+        assertEquals("The event must start before the event ends", resultString);
     }
 
     /**
@@ -104,7 +104,7 @@ public class EventServiceTest {
                 .endTime("19:00")
                 .build();
         String resultString = eventService.verifyEvent(newEvent);
-        assertEquals(resultString, "The end time cannot be before the start time when event is on the same day");
+        assertEquals("The event must start before the event ends", resultString);
     }
 
     /**
@@ -120,6 +120,6 @@ public class EventServiceTest {
                 .endTime("21:00")
                 .build();
         String resultString = eventService.verifyEvent(newEvent);
-        assertEquals(resultString, "Event has been verified");
+        assertEquals("Event has been verified", resultString);
     }
 }
