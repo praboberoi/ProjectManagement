d3b728d5749506176ef1e2cb36b5e932d829aa69
Rory Holmes <rho66@uclive.ac.nz>
2022-07-29 10:12:30 +1200

    #feature Created Deadline entity object and the Deadline Repository class

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/Deadline.java
@@ -0,0 +1,184 @@
+package nz.ac.canterbury.seng302.portfolio.model;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+/**
+ * The data class for Deadlines. Contains the id, name, and date of the deadline for storage in the db.
+ */
+@Entity
+public class Deadline {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private int deadlineID;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "projectId", nullable = false)
+    private Project project;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Date date;
+
+    @Column(nullable = false)
+    private String time;
+
+    /**
+     * No args Constructor for the Deadline.
+     */
+    public Deadline() {}
+
+    /**
+     * Creates a Deadline with an auto-generated ID
+     * @param name The name of the deadline
+     * @param project Project associated with the deadline
+     * @param date Date of the deadline
+     * @param time Time of the deadline
+     */
+    public Deadline(String name, Project project, Date date, String time){
+        this.name = name;
+        this.project = project;
+        this.date = date;
+        this.time = time;
+    }
+
+    /**
+     * Creates a Deadline with an manually generated ID
+     * @param name The name of the deadline
+     * @param project Project associated with the deadline
+     * @param date Date of the deadline
+     * @param time Time of the deadline
+     */
+    public Deadline(int deadlineId, String name, Project project, Date date, String time) {
+        this.deadlineID = deadlineId;
+        this.name = name;
+        this.project = project;
+        this.date = date;
+        this.time = time;
+    }
+
+
+    public int getDeadlineID() {
+        return deadlineID;
+    }
+
+    public void setDeadlineID(int deadlineID) {
+        this.deadlineID = deadlineID;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Deadline deadline)) return false;
+
+        return deadlineID == deadline.deadlineID
+                && name.equals(deadline.name)
+                && date.equals(deadline.date)
+                && time.equals(deadline.time);
+    }
+
+    /**
+     * Builder class to build a deadline
+     */
+    public static class Builder {
+        private int deadlineId;
+        private String name;
+        private Project project;
+        private Date date;
+        private String time;
+
+        /**
+         * Builds the builder with the current deadlineId
+         * @param deadlineId ID for the deadline
+         * @return The current builder
+         */
+        public Builder deadlineId(int deadlineId) {
+            this.deadlineId = deadlineId;
+            return this;
+        }
+
+        /**
+         * Builds the builder with the current deadline's name
+         * @param name Name for the deadline
+         * @return The current builder
+         */
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        /**
+         * Builds the builder with the current project
+         * @param project Project for the deadline
+         * @return The current builder
+         */
+        public Builder project(Project project) {
+            this.project = project;
+            return this;
+        }
+
+        /**
+         * Builds the builder with the current date
+         * @param date Date of the deadline
+         * @return The current builder
+         */
+        public Builder date(Date date) {
+            this.date = date;
+            return this;
+        }
+
+        /**
+         * Builds the builder with the current time
+         * @param time Time of the deadline
+         * @return The current builder
+         */
+        public Builder time(String time) {
+            this.time = time;
+            return this;
+        }
+
+        /**
+         * Returns a new Deadline object with the all the parameters of the current builder
+         * @return Object of type Deadline
+         */
+        public Deadline build() {
+            return new Deadline(deadlineId, name, project, date, time);
+        }
+    }
+}
---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/DeadlineRepository.java
@@ -0,0 +1,32 @@
+package nz.ac.canterbury.seng302.portfolio.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DeadlineRepository extends CrudRepository<Deadline, Integer> {
+
+    /**
+     * Get a list of deadlines that are stored with the given name
+     * @param deadlineName Name of the deadline(s)
+     * @return List of deadline(s) with the given name
+     */
+    List<Deadline> findByName(String deadlineName);
+
+    /**
+     * Obtains a list of deadlines in the given project.
+     * @param project Project containing deadlines
+     * @return List of deadline(s) with the given project
+     */
+    List<Deadline> findByProject(Project project);
+
+    /**
+     * Counts the deadlines based on the given project
+     * @param project Project containing deadline(s)
+     * @return A count of deadline(s) within the project
+     */
+    int countByProject(Project project);
+
+}
