cc408a2acd89921a1a600d83ce8ff28566642cb6
ycr14 <ycr14@uclive.ac.nz>
2022-07-29 15:59:18 +1200

    #feature Working on deadline service class

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/DeadlineService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/DeadlineService.java
@@ -1,16 +1,40 @@
 package nz.ac.canterbury.seng302.portfolio.service;
 
 import nz.ac.canterbury.seng302.portfolio.model.Deadline;
+import nz.ac.canterbury.seng302.portfolio.model.DeadlineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Client service used to communicate to the database and perform business logic for Deadlines
  */
 @Service
 public class DeadlineService {
+    @Autowired private DeadlineRepository deadlineRepository;
 
+    /**
+     * Creates a new deadline
+     * @return
+     */
     public Deadline getNewDeadline() {
-        Deadline deadline = new Deadline.Builder()
+        Deadline deadline = new Deadline.Builder().build();
+        return deadline;
+    }
+
+    /**
+     * Gets deadline object from the database
+     * @param deadlineId of type int
+     * @return of type Deadline
+     * @throws IncorrectDetailsException if given Deadline ID does not exist
+     */
+    public Deadline getDeadline(int deadlineId) throws IncorrectDetailsException{
+        Optional<Deadline> result = deadlineRepository.findById(deadlineId);
+        if(result.isPresent())
+            return result.get();
+        else
+            throw new IncorrectDetailsException("Failed to locate deadline in the database");
     }
 
 
