022240c868b770abf61e24c61039f55de8a81f94
Saskia van der Peet <sva73@uclive.ac.nz>
2022-08-02 13:54:13 +1200

    #feature create the groups page and add a modal to add details to create a group.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupController.java
@@ -1,17 +1,22 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import nz.ac.canterbury.seng302.portfolio.service.GroupService;
 import nz.ac.canterbury.seng302.portfolio.utils.PrincipalUtils;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import nz.ac.canterbury.seng302.shared.identityprovider.DeleteGroupResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Controller for group page
@@ -21,12 +26,45 @@ public class GroupController {
     @Autowired
     private GroupService groupService;
 
+    @Autowired private UserAccountClientService userAccountClientService;
+
+    @Value("${apiPrefix}") private String apiPrefix;
+
+    /**
+     * Adds common model elements used by all controller methods.
+     */
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("apiPrefix", apiPrefix);
+    }
+
+
+    /**
+     * Get message for empty registration page
+     * @param request HTTP request sent to this endpoint
+     * @param response HTTP response that will be returned by this endpoint
+     * @param model Parameters sent to thymeleaf template to be rendered into HTML
+     * @return Registration html page
+     */
+    @GetMapping(path="/groups")
+    public String groups(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Model model,
+            @AuthenticationPrincipal AuthState principal
+
+    ) {
+        model.addAttribute("roles", PrincipalUtils.getUserRole(principal));
+        model.addAttribute("user", userAccountClientService.getUser(principal));
+        return "groups";
+    }
+
 
     /**
      * Attempts to delete a group from the idp server
      * @param groupId The id of the group to be deleted
      * @param principal Authentication information containing user info
-     * @return Status of the request and corrosponding message
+     * @return Status of the request and corresponding message
      */
     @DeleteMapping(value = "/groups/{groupId}/delete")
     public ResponseEntity<String> removeRole(@PathVariable int groupId, @AuthenticationPrincipal AuthState principal) {
