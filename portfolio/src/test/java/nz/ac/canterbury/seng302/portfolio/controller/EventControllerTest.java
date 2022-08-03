6c0bc9243e95c8e9daf0dc017b3a8e7a20d8b120
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-08-03 21:25:22 +1200

    #Test Fix up the new tests created by Leo and James

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/EventControllerTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/EventControllerTest.java
@@ -3,12 +3,16 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 
 import com.google.protobuf.Timestamp;
 import nz.ac.canterbury.seng302.portfolio.model.Event;
+import nz.ac.canterbury.seng302.portfolio.model.EventRepository;
 import nz.ac.canterbury.seng302.portfolio.model.Project;
 import nz.ac.canterbury.seng302.portfolio.model.User;
 import nz.ac.canterbury.seng302.portfolio.service.EventService;
 import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
 import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
+import nz.ac.canterbury.seng302.portfolio.utils.PrincipalUtils;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,8 +25,8 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -44,6 +48,9 @@ public class EventControllerTest {
     @MockBean
     private UserAccountClientService userAccountClientService;
 
+    @MockBean
+    private EventRepository eventRepository;
+
     Event event;
 
     User user;
@@ -52,8 +59,14 @@ public class EventControllerTest {
 
     UserResponse.Builder userResponse;
 
+    @BeforeAll
+    private static void initStaticMocks() {
+        mockStatic(PrincipalUtils.class);
+    }
+
     @BeforeEach
     public void init() {
+        when(PrincipalUtils.checkUserIsTeacherOrAdmin(any())).thenReturn(true);
         LocalDate now = LocalDate.now();
         project = new Project(1, "Test Project", "test", java.sql.Date.valueOf(now), java.sql.Date.valueOf(now.plusDays(50)));
         event = new Event.Builder()
@@ -84,7 +97,10 @@ public class EventControllerTest {
                 .build());
     }
 
-
+    /**
+     * Tests that the new event form is created with the required information
+     * @throws Exception Thrown during mockmvc run time 
+     */
     @Test
     public void givenServer_WhenNavigateToNewEventForm_ThenEventFormReturned() throws Exception{
         when(projectService.getProjectById(1)).thenReturn(project);
@@ -95,7 +111,7 @@ public class EventControllerTest {
                 .andExpect(model().attribute("project", project))
                 .andExpect(model().attribute("event", event))
                 .andExpect(model().attribute("pageTitle", "Add New Event"))
-                .andExpect(model().attribute("user", userResponse))
+                .andExpect(model().attribute("user", userResponse.build()))
                 .andExpect(model().attribute("projectDateMin", project.getStartDate()))
                 .andExpect(model().attribute("projectDateMax", project.getEndDate()));
     }
