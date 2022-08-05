749c134f7a2114c76fe33ee2031fe7e1063c832a
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-08-05 14:35:58 +1200

    #Document Adds documentation to the controller advisor

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/utils/ControllerAdvisor.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/utils/ControllerAdvisor.java
@@ -11,6 +11,9 @@ import nz.ac.canterbury.seng302.portfolio.model.User;
 import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 
+/**
+ * Global utility class to perform any actions that are required by all controller classes
+ */
 @ControllerAdvice
 public class ControllerAdvisor {
     @Autowired 
@@ -18,6 +21,11 @@ public class ControllerAdvisor {
     @Value("${apiPrefix}") 
     private String apiPrefix;
 
+    /**
+     * Adds attributes that are required by all controllers in our application
+     * @param principal Information about the currently logged in user
+     * @param model The model to add sttributes to
+     */
     @ModelAttribute
     public void addAttributes(@AuthenticationPrincipal AuthState principal, Model model) {
         model.addAttribute("apiPrefix", apiPrefix);
