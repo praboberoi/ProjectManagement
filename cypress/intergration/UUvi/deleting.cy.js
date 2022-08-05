38e60f78c5a91d3e8906fef814191e31c0c3616b
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-08-05 13:58:21 +1200

    #Test Fix cypress test issue with multiple user pages

--- cypress/intergration/UUvi/deleting.cy.js
+++ cypress/intergration/UUvi/deleting.cy.js
@@ -4,10 +4,29 @@ When("I remove a {word} role from user {word}", (role, user) => {
     cy.intercept({
         method: 'DELETE',
         url: '/usersList/removeRole',
-      }).as('deleteCheck')
-    cy.contains(user).siblings().contains("button", role).click()
-    cy.wait("@deleteCheck")
-    cy.get(".alert-success")
+    }).as('deleteCheck')
+
+    cy.intercept({
+        method: 'GET',
+        url: '/usersList?*',
+    }).as('nextPage')
+
+    let checkForUser = () => {
+        cy.get("#userListDataTable").then($dataTable => {
+            if ($dataTable.find(':contains(' + user + ')').length == 0) {
+                cy.get("#next-page").click()
+                cy.wait("@nextPage")
+                checkForUser()
+            } else {
+                cy.contains(user).siblings().contains("button", role).click()
+                cy.wait("@deleteCheck")
+                cy.get(".alert-success")
+            }
+        })
+    }
+
+    checkForUser()
+
 });
 
 Then("User {word} isn't a {word}", (user, role) => {
