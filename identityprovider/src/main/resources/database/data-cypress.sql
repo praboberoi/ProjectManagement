1f072183c8c9728989b2c97079f0b23777e39aae
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-08-08 12:12:03 +1200

    #Fix Add h2 compatibility

--- identityprovider/src/main/resources/database/data-cypress.sql
+++ identityprovider/src/main/resources/database/data-cypress.sql
@@ -4,11 +4,11 @@ WHERE user_user_id IN (SELECT user_id FROM user WHERE username LIKE 'Cypress%');
 DELETE FROM user WHERE username LIKE 'Cypress%';
 
 -- Create admin user, large user id to reduce change of overlap
-MERGE INTO user (user_id, bio,date_created,email,first_name,last_name,nickname,password,profile_image_path,pronouns,salt,username) VALUES (1285321, 'Automated test admin','2022-07-15 21:45:57.287000000','automated@cypress.com','Cypress','Admin','','JRqHHIu2hllQo8uswun8chNpkSnX2ERuhyhToKmSdYc=',null,'','aNAKNt2LHIfchQoLQ0Aryg==','CypressAdmin');
-MERGE INTO user_roles (user_user_id,roles,order_column) VALUES (1285321,0,0);
-MERGE INTO user_roles (user_user_id,roles,order_column) VALUES (1285321,1,1);
-MERGE INTO user_roles (user_user_id,roles,order_column) VALUES (1285321,2,2);
+INSERT IGNORE INTO user (user_id, bio,date_created,email,first_name,last_name,nickname,password,profile_image_path,pronouns,salt,username) VALUES (1285321, 'Automated test admin','2022-07-15 21:45:57.287000000','automated@cypress.com','Cypress','Admin','','JRqHHIu2hllQo8uswun8chNpkSnX2ERuhyhToKmSdYc=',null,'','aNAKNt2LHIfchQoLQ0Aryg==','CypressAdmin');
+INSERT IGNORE INTO user_roles (user_user_id,roles,order_column) VALUES (1285321,0,0);
+INSERT IGNORE INTO user_roles (user_user_id,roles,order_column) VALUES (1285321,1,1);
+INSERT IGNORE INTO user_roles (user_user_id,roles,order_column) VALUES (1285321,2,2);
 
-MERGE INTO user (user_id, bio,date_created,email,first_name,last_name,nickname,password,profile_image_path,pronouns,salt,username) VALUES (1285322, 'Automated test guinea pig','2022-07-15 21:45:57.287000000','automated@cypress.com','Guinea','pig','','JRqHHIu2hllQo8uswun8chNpkSnX2ERuhyhToKmSdYc=',null,'','aNAKNt2LHIfchQoLQ0Aryg==','CypressGuineaPig');
-MERGE INTO user_roles (user_user_id,roles,order_column) VALUES (1285322,0,0);
-MERGE INTO user_roles (user_user_id,roles,order_column) VALUES (1285322,2,1);
+INSERT IGNORE INTO user (user_id, bio,date_created,email,first_name,last_name,nickname,password,profile_image_path,pronouns,salt,username) VALUES (1285322, 'Automated test guinea pig','2022-07-15 21:45:57.287000000','automated@cypress.com','Guinea','pig','','JRqHHIu2hllQo8uswun8chNpkSnX2ERuhyhToKmSdYc=',null,'','aNAKNt2LHIfchQoLQ0Aryg==','CypressGuineaPig');
+INSERT IGNORE INTO user_roles (user_user_id,roles,order_column) VALUES (1285322,0,0);
+INSERT IGNORE INTO user_roles (user_user_id,roles,order_column) VALUES (1285322,2,1);
