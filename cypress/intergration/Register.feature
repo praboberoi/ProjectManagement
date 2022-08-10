1be410fbe39a191349768f3ea8c37c0785c867fc
Rory Holmes <rho66@uclive.ac.nz>
2022-08-10 14:25:08 +1200

    #test Created part of an acceptance test for registering an account

--- cypress/intergration/Register.feature
+++ cypress/intergration/Register.feature
@@ -2,3 +2,7 @@ Feature: UUi Registering and logging into a user account
   Scenario: Creating a valid user
     When I register a new user
     Then I can login
+
+  Scenario: Appropriate validation is carried out and errors are clearly conveyed.
+    When I enter an invalid email
+    Then An error message is displayed
--- cypress/intergration/register/register.cy.js
+++ cypress/intergration/register/register.cy.js
@@ -19,3 +19,21 @@ Then("I can login", () => {
     cy.login("CypressRegister", "CypressUser1")
     cy.url().should('eq', Cypress.config().baseUrl + '/dashboard')
 });
+
+When("I enter an invalid email", () => {
+    cy.visit("")
+    cy.get("#register").click()
+    cy.get('#username').type('CypressRegister')
+    cy.get('#firstName').type('Cypress')
+    cy.get('#lastName').type('Register')
+    cy.get('#nickname').type('Cypress')
+    cy.get('#email').type('automated@')
+    cy.get('#password').type('CypressUser1')
+    cy.get('#confirmPassword').type('CypressUser1')
+    cy.get("#submit").click()
+});
+
+Then("I can login", () => {
+    cy.login("CypressRegister", "CypressUser1")
+    cy.url().should('eq', Cypress.config().baseUrl + '/dashboard')
+});
