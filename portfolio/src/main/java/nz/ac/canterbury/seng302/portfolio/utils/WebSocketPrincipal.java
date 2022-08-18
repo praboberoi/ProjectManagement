def41d211b1eee1909b748ffa35fe56f3f4281e4
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-08-19 10:32:48 +1200

    #Document add missing documentation

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/utils/WebSocketPrincipal.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/utils/WebSocketPrincipal.java
@@ -2,7 +2,9 @@ package nz.ac.canterbury.seng302.portfolio.utils;
 
 import java.security.Principal;
 
-
+/**
+ * New principal to override the authstate one that is being used and provide a name for the user
+ */
 public class WebSocketPrincipal implements Principal{
 
     Principal authState = null;
@@ -16,6 +18,10 @@ public class WebSocketPrincipal implements Principal{
         return "User";
     }
 
+    /**
+     * Gets the authstate principal that this is replacing
+     * @return
+     */
     public Principal getPrincipal(){
         return authState;
     }
