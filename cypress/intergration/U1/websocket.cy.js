a05f36da19ca2120f3214203107ab9a168bf3bb2
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-08-24 01:41:06 +0000

    Update project page on changes

---
+++ cypress/intergration/U1/websocket.cy.js
@@ -0,0 +1,33 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+When("I call delete event on {string}", (event) => {
+    cy.getCookie("lens-session-token").then((cookie) => {
+        cy.setCookie('lens-session-token', cookie.value)
+    })
+    cy.get("#projectId").invoke('val').then((projectId) => {
+        cy.contains(event).parents('.event-card').find(".hiddenEventId").invoke('val').then((eventId) => {
+            cy.request('DELETE', Cypress.config().baseUrl + '/project/' + projectId + '/event/' + eventId + '/delete')
+        })
+    })
+})
+
+When("I call edit event on {string}", (event) => {
+    cy.getCookie("lens-session-token").then((cookie) => {
+        cy.setCookie('lens-session-token', cookie.value)
+    })
+    cy.get("#projectId").invoke('val').then((projectId) => {
+        cy.contains(event).parents('.event-card').find(".hiddenEventId").invoke('val').then((eventId) => {
+            cy.request('POST', Cypress.config().baseUrl + '/project/' + projectId + '/saveEvent?eventId=' + eventId + '&eventName=Cypress+Websocket+Edited+Event&startDate=2022-07-17T00%3A00&endDate=2022-07-20T00%3A00')
+        })
+    })
+})
+
+When("I call create event on {string}", (event) => {
+    cy.getCookie("lens-session-token").then((cookie) => {
+        cy.setCookie('lens-session-token', cookie.value)
+    })
+    cy.get("#projectId").invoke('val').then((projectId) => {
+        cy.request('POST', Cypress.config().baseUrl + '/project/' + projectId + '/saveEvent?eventName=' + event.replaceAll(' ', '+') + '&startDate=2022-07-17T00%3A00&endDate=2022-07-20T00%3A00')
+
+    })
+})
