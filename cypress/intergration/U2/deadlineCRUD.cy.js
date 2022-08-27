00f20237f3484529ffac8d546acf07b8ec6ad250
Prableen Oberoi <pob16@uclive.ac.nz>
2022-08-27 21:23:24 +1200

    # tests modified some tests to improve the response time for bulk running

--- cypress/intergration/U2/deadlineCRUD.cy.js
+++ cypress/intergration/U2/deadlineCRUD.cy.js
@@ -9,11 +9,11 @@ Given("I select Create Deadline", () => {
 })
 
 When("I enter {string} deadline name and {string} date", (deadlineName, deadlineDate) => {
-    cy.get('#deadline-name').clear()
+    cy.get('#deadline-name').clear().should('have.value', '')
     cy.get('#deadlineNameError').should('have.text', 'Deadline Name must not be empty')
-    cy.get('#deadline-name').type(deadlineName)
+    cy.get('#deadline-name').type(deadlineName).wait(200).should('have.value', deadlineName)
     cy.get('#deadlineNameError').should('have.text', '')
-    cy.get('#deadlineDate').type(deadlineDate)
+    cy.get('#deadlineDate').type(deadlineDate).should('have.value', deadlineDate)
 })
 
 When('I select save on the deadline form',  () => {
@@ -21,16 +21,16 @@ When('I select save on the deadline form',  () => {
 })
 
 When("I select edit for {string} deadline", (deadline) => {
-    cy.get(`[data-deadlinename="${deadline}"]`).contains(" Edit").click()
+    cy.contains(`[data-deadlinename="${deadline}"]`, " Edit").click()
 })
 
 When("I change the name to {string}", (deadlineName) => {
-    cy.get('#deadline-name').clear()
-    cy.get('#deadline-name').type(deadlineName)
+    cy.get('#deadline-name').clear().should('have.value','')
+    cy.get('#deadline-name').type(deadlineName).should('have.value', deadlineName)
 })
 
 When("I select delete for {string} deadline", (deadlineName) => {
-    cy.get(`[data-deadlinename="${deadlineName}"]`).contains(" Delete").click()
+    cy.contains(`[data-deadlinename="${deadlineName}"]`, " Delete").click()
 })
 
 When('I select delete again on the conformation modal', ()=> {
@@ -38,18 +38,18 @@ When('I select delete again on the conformation modal', ()=> {
 })
 
 Then('A new deadline is created', () => {
-    cy.contains('Successfully Created Test 1')
+    cy.contains("#messageSuccess",'Successfully Created Test 1')
     cy.get('#deadline-tab').click()
     cy.get('.card-title').should('contain.text', 'Test 1')
     cy.visit('/logout')
 })
 
 Then("Deadline name is updated successfully to {string}", (deadlineName) => {
-    cy.contains(`Successfully Updated ${deadlineName}`)
+    cy.contains("#messageSuccess",`Successfully Updated ${deadlineName}`)
     cy.visit('/logout')
 })
 
 Then("{string} deadline is successfully deleted", (deadlineName) => {
-    cy.contains(`Successfully deleted ${deadlineName}`)
+    cy.contains("#messageSuccess",`Successfully deleted ${deadlineName}`)
     cy.visit('/logout')
 })
