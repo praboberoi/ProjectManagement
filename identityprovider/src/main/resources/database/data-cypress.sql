17e60c7058040631987f4be563c500bee4b9b380
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-09-01 15:29:53 +1200

    #Fix Change cypress user id's to decrease

--- identityprovider/src/main/resources/database/data-cypress.sql
+++ identityprovider/src/main/resources/database/data-cypress.sql
@@ -30,10 +30,10 @@ INSERT IGNORE INTO user (user_id, bio,date_created,email,first_name,last_name,ni
 INSERT IGNORE INTO user_roles (user_user_id,roles,order_column) VALUES (1285322,0,0);
 INSERT IGNORE INTO user_roles (user_user_id,roles,order_column) VALUES (1285322,2,1);
 -- U5 test users
-INSERT IGNORE INTO user (user_id, bio,date_created,email,first_name,last_name,nickname,password,profile_image_path,pronouns,salt,username) VALUES (1285323, 'Automated test guinea pig','2022-07-15 21:45:57.287000000','automated@cypress.com','Guinea','pig','','JRqHHIu2hllQo8uswun8chNpkSnX2ERuhyhToKmSdYc=',null,'','aNAKNt2LHIfchQoLQ0Aryg==','CypressGroupGuineaPig');
+INSERT IGNORE INTO user (user_id, bio,date_created,email,first_name,last_name,nickname,password,profile_image_path,pronouns,salt,username) VALUES (1285321, 'Automated test guinea pig','2022-07-15 21:45:57.287000000','automated@cypress.com','Guinea','pig','','JRqHHIu2hllQo8uswun8chNpkSnX2ERuhyhToKmSdYc=',null,'','aNAKNt2LHIfchQoLQ0Aryg==','CypressGroupGuineaPig');
 INSERT IGNORE INTO user_roles (user_user_id,roles,order_column) VALUES (1285323,0,0);
 INSERT IGNORE INTO user_roles (user_user_id,roles,order_column) VALUES (1285323,1,1);
-INSERT IGNORE INTO user (user_id, bio,date_created,email,first_name,last_name,nickname,password,profile_image_path,pronouns,salt,username) VALUES (1285324, 'Automated test guinea pig','2022-07-15 21:45:57.287000000','automated@cypress.com','Guinea','pig','','JRqHHIu2hllQo8uswun8chNpkSnX2ERuhyhToKmSdYc=',null,'','aNAKNt2LHIfchQoLQ0Aryg==','CypressUnassignedGuineaPig');
+INSERT IGNORE INTO user (user_id, bio,date_created,email,first_name,last_name,nickname,password,profile_image_path,pronouns,salt,username) VALUES (1285320, 'Automated test guinea pig','2022-07-15 21:45:57.287000000','automated@cypress.com','Guinea','pig','','JRqHHIu2hllQo8uswun8chNpkSnX2ERuhyhToKmSdYc=',null,'','aNAKNt2LHIfchQoLQ0Aryg==','CypressUnassignedGuineaPig');
 INSERT IGNORE INTO user_roles (user_user_id,roles,order_column) VALUES (1285324,0,0);
 
 -- Create group and add member into it
