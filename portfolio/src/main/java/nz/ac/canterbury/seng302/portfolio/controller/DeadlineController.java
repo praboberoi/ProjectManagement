eeb61db4d7036ab67ca0c477fdcef2a1294964a6
James Claridge <jcl165@uclive.ac.nz>
2022-09-01 16:09:21 +1200

    Created CustomHandshakeHandler using Jack's code as a reference. Changed DeadlineController to work with web sockets. Changed WebSocketConfig to created web sockets. #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/DeadlineController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/DeadlineController.java
@@ -2,46 +2,79 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 import nz.ac.canterbury.seng302.portfolio.model.Deadline;
 import nz.ac.canterbury.seng302.portfolio.model.Event;
 import nz.ac.canterbury.seng302.portfolio.model.Project;
+import nz.ac.canterbury.seng302.portfolio.model.notifications.DeadlineNotification;
 import nz.ac.canterbury.seng302.portfolio.service.DeadlineService;
 import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
 import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
 import nz.ac.canterbury.seng302.portfolio.utils.IncorrectDetailsException;
 import nz.ac.canterbury.seng302.portfolio.utils.PrincipalUtils;
+import nz.ac.canterbury.seng302.portfolio.utils.WebSocketPrincipal;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 /**
  * Controller for the deadlines
  */
 @Controller
 public class DeadlineController {
-    @Value("${apiPrefix}")
-    private String apiPrefix;
     @Autowired
     private DeadlineService deadlineService;
     @Autowired
     private ProjectService projectService;
     @Autowired
     private UserAccountClientService userAccountClientService;
-    private final static String RedirectToProjectPage = "redirect:/project/{projectId}";
+    @Autowired
+    private SimpMessagingTemplate template;
+    private Logger logger = LoggerFactory.getLogger(DeadlineController.class);
+    private static Set<DeadlineNotification> editing = new HashSet<>();
+    private static final String PROJECT_REDIRECT = "redirect:/project/{projectId}";
+    private static final String SUCCESS_MESSAGE = "messageSuccess";
+    private static final String FAILURE_MESSAGE = "messageDanger";
+
+
     public DeadlineController(DeadlineService deadlineService, ProjectService projectService) {
         this.deadlineService = deadlineService;
         this.projectService = projectService;
     }
+
     /**
-     * Adds common model elements used by all controller methods.
+     * Return the html component which contains the specified project's deadlines
+     * @param projectId Project containing the desired deadlines
+     * @return Page fragment containing deadlines
      */
-    @ModelAttribute
-    public void addAttributes(Model model) {
-        model.addAttribute("apiPrefix", apiPrefix);
+    @GetMapping(path="/project/{projectId}/deadlines")
+    public ModelAndView events(@PathVariable("projectId") int projectId) {
+        List<Deadline> listDeadlines = deadlineService.getDeadlineByProject(projectId);
+        Project project = new Project();
+        project.setProjectId(projectId);
+        ModelAndView mv = new ModelAndView("project::deadlines");
+        mv.addObject("project", project);
+        mv.addObject("listDeadlines", listDeadlines);
+        return mv;
+    }
+
+    private void notifyDeadline(int projectId, int deadlineId, String action) {
+        template.convertAndSend("element/project/" + projectId + "/deadlines", ("deadline" + deadlineId + " " + action));
     }
 
     /**
@@ -64,11 +97,14 @@ public class DeadlineController {
             deadline.setProject(projectService.getProjectById(projectId));
             deadlineService.verifyDeadline(deadline);
             message = deadlineService.saveDeadline(deadline);
-            ra.addFlashAttribute("messageSuccess", message);
+            logger.info("Deadline {} has been edited", deadline.getDeadlineId());
+            notifyDeadline(projectId, deadline.getDeadlineId(), "edited");
+            ra.addFlashAttribute(SUCCESS_MESSAGE, message);
         } catch (IncorrectDetailsException ex) {
-            ra.addFlashAttribute("messageDanger", ex.getMessage());
+            logger.info("Deadline {} could not be edited", deadline.getDeadlineId());
+            ra.addFlashAttribute(FAILURE_MESSAGE, ex.getMessage());
         }
-        return RedirectToProjectPage;
+        return PROJECT_REDIRECT;
     }
     /**
      * Deletes the deadline and redirects back to project page
@@ -89,46 +125,51 @@ public class DeadlineController {
         if (!PrincipalUtils.checkUserIsTeacherOrAdmin(principal)) return "redirect:/dashboard";
         try {
             String message = deadlineService.deleteDeadline(deadlineId);
-            ra.addFlashAttribute("messageSuccess", message);
+            logger.info("Deadline {} has been deleted.", deadlineId);
+            ra.addFlashAttribute(SUCCESS_MESSAGE, message);
+            notifyDeadline(projectId, deadlineId, "deleted");
+            // TODO REMOVE ONCE WEB SOCKETS WORKING
             List<Deadline> listDeadlines = deadlineService.getDeadlineByProject(projectId);
             model.addAttribute("listDeadlines", listDeadlines);
-            return RedirectToProjectPage;
+            return PROJECT_REDIRECT;
         } catch (IncorrectDetailsException e) {
+            logger.info("Deadline {} could not be deleted.", deadlineId);
             ra.addFlashAttribute("messageDanger", e.getMessage());
-            return RedirectToProjectPage;
+            return PROJECT_REDIRECT;
         }
     }
+
     /**
-     * Maps an existing deadline, current user, user's role and button info to the deadline form
-     * @param deadlineId Of type int
-     * @param projectId Of type int
-     * @param model Of type {@link Model}
-     * @param principal Of type {@link AuthState}
-     * @param ra Of type {@link RedirectAttributes}
-     * @return deadlineForm.html or project.html
+     * Sends an update message to every client subscribed to /deadline/edit when a user starts editing a deadline
+     * @param notification Notification containing the deadline ID and project ID that is being edited
+     * @param principal Authentication information containing user info
+     * @param sessionId Session ID of the websocket communication
      */
-    @GetMapping(path="/project/{projectId}/editDeadline/{deadlineId}")
-    public String deadlineEditForm(
-            @PathVariable("deadlineId") int deadlineId,
-            @PathVariable("projectId") int projectId,
-            Model model,
-            @AuthenticationPrincipal AuthState principal,
-            RedirectAttributes ra){
-        if (!PrincipalUtils.checkUserIsTeacherOrAdmin(principal)) return RedirectToProjectPage;
-        try {
-            Project currentProject = projectService.getProjectById(projectId);
-            Deadline deadline = deadlineService.getDeadline(deadlineId);
-            model.addAttribute("deadline", deadline);
-            model.addAttribute("project", currentProject);
-            model.addAttribute("pageTitle", "Edit Deadline: " + deadline.getName());
-            model.addAttribute("deadlineMin", currentProject.getStartDate());
-            model.addAttribute("deadlineMax", currentProject.getEndDate());
-            model.addAttribute("submissionName", "Save");
-            model.addAttribute("image", apiPrefix + "/icons/save-icon.svg");
-            return "deadlineForm";
-        } catch (IncorrectDetailsException e) {
-            ra.addFlashAttribute("messageDanger", e.getMessage());
-            return RedirectToProjectPage;
+    @MessageMapping("/deadline/edit")
+    public void editing(DeadlineNotification notification, @AuthenticationPrincipal WebSocketPrincipal principal, @Header("simpSessionId") String sessionId) {
+        notification.setUsername(principal.getName());
+        notification.setSessionId(sessionId);
+        if (notification.isActive()) {
+            template.convertAndSend("element/project/" + notification.getProjectId() + "/deadlines",
+                    ("deadline" + notification.getDeadlineId() + " editing " + notification.getUsername()));
+            editing.add(notification);
+        } else {
+            template.convertAndSend("element/project/" + notification.getProjectId() + "/deadlines",
+                    ("deadline" + notification.getDeadlineId() + " finished"));
+            editing.remove(notification);
         }
     }
+
+    /**
+     * Detects when a websocket disconnects to remove them from the list of editors
+     * @param event Websocket disconnect event
+     */
+    @EventListener
+    public void onApplicationEvent(SessionDisconnectEvent event) {
+        editing.stream().filter(notification -> notification.getSessionId().equals(event.getSessionId()))
+                .forEach(notification -> template.convertAndSend("/element/project/" + notification.getProjectId() +
+                        "/deadlines", ("deadline" + notification.getDeadlineId() + " finished")));
+        editing.removeIf(notification -> notification.getSessionId().equals(event.getSessionId()));
+    }
+
 }
