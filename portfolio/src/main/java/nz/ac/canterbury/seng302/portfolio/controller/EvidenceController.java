ee33a881297c1ec14713192ddfc00c99220f334d
Rory Holmes <rho66@uclive.ac.nz>
2022-09-07 14:30:27 +1200

    #Feature Added in a method for retrieving a list of evidence objects and a method for retrieving a specific evidence object.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EvidenceController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EvidenceController.java
@@ -1,7 +1,5 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
-import nz.ac.canterbury.seng302.portfolio.model.Event;
-import nz.ac.canterbury.seng302.portfolio.model.Groups;
 import nz.ac.canterbury.seng302.portfolio.model.Project;
 import nz.ac.canterbury.seng302.portfolio.model.dto.EvidenceDTO;
 import nz.ac.canterbury.seng302.portfolio.model.Evidence;
@@ -18,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -28,18 +27,21 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * Controller for the evidence page
  */
 @Controller
 public class EvidenceController {
-
+    @Value("${apiPrefix}")
+    private String apiPrefix;
     @Autowired
     private EvidenceService evidenceService;
+    private final static String RedirectToAccountPage = "redirect:/account";
 
-    @Value("${apiPrefix}") private String apiPrefix;
+    public EvidenceController(EvidenceService evidenceService) {
+        this.evidenceService = evidenceService;
+    }
 
     /**
      * Adds common model elements used by all controller methods.
@@ -50,30 +52,42 @@ public class EvidenceController {
     }
 
     /**
-     * Get message for empty registration page
-     * @param request HTTP request sent to this endpoint
-     * @param response HTTP response that will be returned by this endpoint
-     * @param model Parameters sent to thymeleaf template to be rendered into HTML
-     * @return Registration html page
+     * Updates the model with the correct list of evidence
+     * @param userId UserId containing the desired evidence
+     * @return Page fragment containing events
      */
     @GetMapping(path="/evidence/{userId}")
-    public String evidence(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Model model,
-            @AuthenticationPrincipal AuthState principal
+    public ModelAndView evidenceList(
+            @PathVariable("userId") int userId,
+            @AuthenticationPrincipal AuthState principal) {
+        List<Evidence> listEvidence = evidenceService.getEvidenceByUserId(userId);
+        ModelAndView mv = new ModelAndView("evidence::evidenceList");
+        mv.addObject("listEvidence", listEvidence);
+        return mv;
+    }
 
-    ) {
-        LocalDate now = LocalDate.now();
-        Project project = new Project(1, "Test Project", "test", java.sql.Date.valueOf(now),
-                java.sql.Date.valueOf(now.plusDays(50)));
-        Evidence evidence1 = new Evidence(1, project, new Date(), "Test Evidence 1", "testing", 1);
-        Evidence evidence2 = new Evidence(2, project, new Date(), "Test Evidence 2", "testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing ", 1);
-        Evidence[] evidences = {evidence1, evidence2};
-        List<Evidence> listEvidences = Arrays.asList(evidences);
-        model.addAttribute("listEvidence", listEvidences);
-        model.addAttribute("selectedEvidence", evidence2);
-        return "evidence";
+    /**
+     * Updates the model with the correct list of evidence and a selected evidence
+     * @param userId User ID containing the evidence
+     * @param evidenceId ID of the selected evidence object
+     * @param ra Redirect Attribute, a frontend message object
+     * @return The selected evidence fragment
+     */
+    @GetMapping(path="/evidence/{userId}/{evidenceId}")
+    public ModelAndView selectedEvidence(
+            @PathVariable int userId,
+            @PathVariable int evidenceId,
+            RedirectAttributes ra) {
+        ModelAndView mv = new ModelAndView("evidence::evidenceList");
+        List<Evidence> listEvidence = evidenceService.getEvidenceByUserId(userId);
+        mv.addObject("listEvidence", listEvidence);
+        try {
+            Evidence selectedEvidence = evidenceService.getEvidence(evidenceId);
+            mv.addObject("selectedEvidence", selectedEvidence);
+        } catch (IncorrectDetailsException e) {
+            ra.addFlashAttribute("messageDanger", e.getMessage());
+        }
+        return mv;
     }
 
 //    /**
@@ -96,18 +110,73 @@ public class EvidenceController {
      * @param ra Redirect Attribute frontend message object
      * @return link of html page to display
      */
-    @PostMapping(path="/evidence/saveEvidence")
+    @PostMapping(path="/evidence/{userId}/saveEvidence")
     public String saveEvidence(
             @ModelAttribute Evidence evidence,
+            @PathVariable("userId") int userId,
             RedirectAttributes ra,
             @AuthenticationPrincipal AuthState principal) {
         try {
-            evidence.setOwnerId(PrincipalUtils.getUserId(principal));
-            String message = "";
+            evidence.setOwnerId(userId);
             evidenceService.verifyEvidence(evidence);
-            message = evidenceService.saveEvidence(evidence);
+            String message = evidenceService.saveEvidence(evidence);
             ra.addFlashAttribute("messageSuccess", message);
         } catch(IncorrectDetailsException e) {
+            ra.addFlashAttribute("messageDanger", e.getMessage());
+        }
+        return "redirect:/evidence";
+    }
+
+    /**
+     * Maps an existing evidence info to the evidence form
+     * @param evidenceId Of type int
+     * @param model Of type {@link Model}
+     * @param principal Of type {@link AuthState}
+     * @param ra Of type {@link RedirectAttributes}
+     * @return evidenceForm.html or project.html
+     */
+    @GetMapping(path="/editEvidence/{evidenceId}")
+    public String evidenceEditForm(
+            @PathVariable("evidenceId") int evidenceId,
+            Model model,
+            @AuthenticationPrincipal AuthState principal,
+            RedirectAttributes ra){
+        if (!PrincipalUtils.checkUserIsTeacherOrAdmin(principal)) return RedirectToAccountPage;
+        try {
+            Evidence evidence = evidenceService.getEvidence(evidenceId);
+            Project currentProject = evidence.getProject();
+            model.addAttribute("evidence", evidence);
+            model.addAttribute("evidenceMin", currentProject.getStartDate());
+            model.addAttribute("evidenceMax", currentProject.getEndDate());
+            return "evidenceForm";
+        } catch (IncorrectDetailsException e) {
+            ra.addFlashAttribute("messageDanger", e.getMessage());
+            return RedirectToAccountPage;
+        }
+    }
+
+    /**
+     * Deletes the evidence and redirects back to the evidence page
+     * @param model Of type {@link Model}
+     * @param evidenceId Of type int
+     * @param principal Of type {@link AuthState}
+     * @param ra Of type {@link RedirectAttributes}
+     * @return project.html or error.html
+     */
+    @PostMapping(path="/deleteEvidence/{evidenceId}")
+    public String deleteEvidence(
+            @PathVariable("evidenceId") int evidenceId,
+            Model model,
+            @AuthenticationPrincipal AuthState principal,
+            RedirectAttributes ra) {
+        if (!PrincipalUtils.checkUserIsTeacherOrAdmin(principal)) return RedirectToAccountPage;
+        try {
+            int userId = PrincipalUtils.getUserId(principal);
+            String message = evidenceService.deleteEvidence(evidenceId);
+            ra.addFlashAttribute("messageSuccess", message);
+            List<Evidence> listEvidence = evidenceService.getEvidenceByUserId(userId);
+            model.addAttribute("listEvidence", listEvidence);
+        } catch (IllegalArgumentException e) {
             ra.addFlashAttribute("messageDanger", e.getMessage());
         }
         return "redirect:/evidence";
