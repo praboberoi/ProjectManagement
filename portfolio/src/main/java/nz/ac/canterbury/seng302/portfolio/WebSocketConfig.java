e48e5c87504a9003610d44785678c005db718336
Prableen Oberoi <pob16@uclive.ac.nz>
2022-09-08 15:45:36 +1200

    #feature programmed the changes for websocket for CORS

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/WebSocketConfig.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/WebSocketConfig.java
@@ -32,7 +32,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/lensfolio-websocket")
-                .setAllowedOriginPatterns("wss://*.canterbury.ac.nz")
+                .setAllowedOriginPatterns("*")
                 .setHandshakeHandler(new CustomHandshakeHandler());
     }
 
