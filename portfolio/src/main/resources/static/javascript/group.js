552c59d8a046013036de683df7bbc653087c8f14
Rory Holmes <rho66@uclive.ac.nz>
2022-09-12 16:46:13 +1200

    #Feature Added in all front end validation for repo settings

--- portfolio/src/main/resources/static/javascript/group.js
+++ portfolio/src/main/resources/static/javascript/group.js
@@ -1,7 +1,18 @@
 let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+let projectIdValidate = /^\d+$/;
 let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+function isValidHttpUrl(string) {
+    let url;
+    try {
+        url = new URL(string)
+    } catch (_) {
+        return false;
+    }
+    return url.protocol === "http:" || url.protocol === "https:"
+}
 
 /**
  * Attempts to connect to the git repository using the details provided
@@ -34,6 +45,57 @@ function connectTest(event) {
         document.getElementById("git-project-name").value = ""
         message.innerText = "Error connecting to repository"
     });
+}
 
-    
+function validateProjectAlias() {
+    let projectAliasElement = document.getElementById("git-project-alias");
+    let projectAliasErrorElement = document.getElementById("gitProjectAliasError");
+
+    if (projectAliasElement.value.length < 1 || projectAliasElement.value.length > 50) {
+        projectAliasElement.classList.add("formError")
+        projectAliasErrorElement.innerText = "Project Alias field must be between 1 and 50 characters"
+        projectAliasElement.setCustomValidity("Invalid field.")
+    } else {
+        projectAliasElement.classList.remove("formError");
+        projectAliasErrorElement.innerText = null;
+        projectAliasElement.setCustomValidity("");
+    }
+}
+
+function validateProjectID() {
+    let projectIDElement = document.getElementById("git-project-id");
+    let projectIDErrorElement = document.getElementById("gitProjectIdError");
+
+    if (projectIDElement.value.length < 1 || projectIDElement.value.length > 50) {
+        projectIDElement.classList.add("formError")
+        projectIDErrorElement.innerText = "Project ID field must be between 1 and 50 characters"
+        projectIDElement.setCustomValidity("Invalid field.")
+    } else if (!projectIdValidate.test(projectIDElement.value)) {
+        projectIDElement.classList.add("formError")
+        projectIDErrorElement.innerText = "Project ID field can only contain numbers"
+        projectIDElement.setCustomValidity("Invalid field.")
+    } else {
+            projectIDElement.classList.remove("formError");
+            projectIDErrorElement.innerText = null;
+            projectIDElement.setCustomValidity("");
+    }
+}
+
+function validateProjectHostAddress() {
+    let projectHostAddressElement = document.getElementById("git-host-address");
+    let projectHostAddressErrorElement = document.getElementById("gitHostAddressError");
+
+    if (projectHostAddressElement.value.length < 1) {
+        projectHostAddressElement.classList.add("formError")
+        projectHostAddressErrorElement.innerText = "Project host address field must not be empty"
+        projectHostAddressElement.setCustomValidity("Invalid field.")
+    } else if (!isValidHttpUrl(projectHostAddressElement.value)) {
+        projectHostAddressElement.classList.add("formError")
+        projectHostAddressErrorElement.innerText = "Project host address must be a valid HTTP URL"
+        projectHostAddressElement.setCustomValidity("Invalid field.")
+    } else {
+        projectHostAddressElement.classList.remove("formError");
+        projectHostAddressErrorElement.innerText = null;
+        projectHostAddressElement.setCustomValidity("");
+    }
 }
