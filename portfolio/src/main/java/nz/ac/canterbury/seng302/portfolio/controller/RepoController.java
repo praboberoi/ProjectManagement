bdb9ccf158fe3155e6c85267f06c92dccf8e0f7f
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-09-12 18:31:04 +1200

    #Test Created tests for getting the repo segment

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/RepoController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/RepoController.java
@@ -3,6 +3,7 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,9 +16,11 @@ import nz.ac.canterbury.seng302.portfolio.model.Repo;
 import nz.ac.canterbury.seng302.portfolio.model.RepoRepository;
 import nz.ac.canterbury.seng302.portfolio.model.dto.RepoDTO;
 import nz.ac.canterbury.seng302.portfolio.service.GroupService;
+import nz.ac.canterbury.seng302.portfolio.utils.PrincipalUtils;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 
 /**
- * Controller for group page
+ * Controller for repo components
  */
 @Controller
 public class RepoController {
@@ -30,23 +33,40 @@ public class RepoController {
 
     /**
      * Gets the groups repo.
-     * @param groupId The pages group id for future implementation
-     * @return The group page
+     * 
+     * @param groupId The group id of the repo to get.
+     * @return The repo page segment
      */
-    @GetMapping(path="/repo/{groupId}")
-    public ModelAndView groupPage(@PathVariable int groupId) {
-        ModelAndView mv = new ModelAndView("groupFragment::repoSettings");
+    @GetMapping(path = "/repo/{groupId}")
+    public ModelAndView groupPage(@PathVariable int groupId, @AuthenticationPrincipal AuthState principal) {
+        Groups group = groupService.getGroupById(groupId);
+        ModelAndView mv;
+        if (group == null || group.getGroupId() == 0) {
+            mv = new ModelAndView("groupFragments::repoSettingsError");
+            mv.setStatus(HttpStatus.NOT_FOUND);
+            return mv;
+        } else if (!(PrincipalUtils.checkUserIsTeacherOrAdmin(principal)) && group.getMembers().stream()
+                .noneMatch(user -> user.getUserId() == PrincipalUtils.getUserId(principal))) {
+            mv = new ModelAndView("groupFragments::repoSettingsError");
+            mv.setStatus(HttpStatus.FORBIDDEN);
+            return mv;
+        }
+
+        Repo repo = repoRepository.getByGroupId(groupId);
+        mv = new ModelAndView("groupFragments::repoSettings");
+        mv.addObject("repo", repo);
         return mv;
     }
 
     /**
      * Gets the individual group page.
+     * 
      * @param groupId The pages group id for future implementation
-     * @param model Parameters sent to thymeleaf template to be rendered into HTML
-     * @param ra Redirect Attribute frontend message object
+     * @param model   Parameters sent to thymeleaf template to be rendered into HTML
+     * @param ra      Redirect Attribute frontend message object
      * @return The group page
      */
-    @PostMapping(path="/repo/{groupId}/save")
+    @PostMapping(path = "/repo/{groupId}/save")
     public ResponseEntity<String> saveGroupRepo(@PathVariable int groupId, @ModelAttribute RepoDTO repoDTO) {
         Groups group = groupService.getGroupById(groupId);
         if (group.getGroupId() == 0) {
