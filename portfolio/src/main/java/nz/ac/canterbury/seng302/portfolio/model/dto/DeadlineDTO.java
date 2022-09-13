8f6cf423eaaf6ff562ea06fcb75241153cdf93c9
Jack McCorkindale <jdm183@uclive.ac.nz>
2022-09-13 23:52:59 +1200

    #Chore Create deadline DTO

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/dto/DeadlineDTO.java
@@ -0,0 +1,44 @@
+package nz.ac.canterbury.seng302.portfolio.model.dto;
+
+import java.util.Date;
+
+import nz.ac.canterbury.seng302.portfolio.model.Project;
+
+/**
+ * The repoDTO entity stored in the database for the portfolio application
+ */
+
+public class DeadlineDTO {
+
+    private int deadlineId;
+
+    private Project project;
+
+    private String name;
+
+    private Date date;
+
+    public int getDeadlineId() {return deadlineId;}
+
+    public Project getProject() {return project;}
+
+    public String getName() {return name;}
+
+    public Date getDate() {return date;}
+
+    /**
+     * Creates a new repoDTO object with the provided details
+     * @param repoId The ID of the repo object
+     * @param groupId GroupId of the repo object
+     * @param repoName Name of the repo object
+     * @param gitlabProjectId The ID of the gitlab project
+     * @param accessToken The access token
+     * @param hostAddress The host address
+     */
+    public DeadlineDTO(int deadlineId, String name, Project project, Date date){
+        this.deadlineId = deadlineId;
+        this.name = name;
+        this.project = project;
+        this.date = date;
+    }
+}
