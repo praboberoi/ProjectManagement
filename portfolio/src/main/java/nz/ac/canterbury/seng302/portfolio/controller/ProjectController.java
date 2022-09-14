bca86848b1308ea5e762662369096cb223147b0c
James Claridge <jcl165@uclive.ac.nz>
2022-09-14 15:15:58 +1200

    Created method in sprint repository to find a sprint that occurs over a given date within a given project. Added method to deadline service that maps the sprints that occur on the deadline to the deadline. Added html to display what sprint is occurring (if any) over the deadline. #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProjectController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProjectController.java
@@ -21,6 +21,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.Date;
+import java.util.Hashtable;
 import java.util.List;
 
 /**
@@ -68,6 +69,7 @@ public class ProjectController {
             List<Sprint> listSprints = sprintService.getSprintByProject(projectId);
             List<Event> listEvents = eventService.getEventByProjectId(projectId);
             List<Deadline> listDeadlines = deadlineService.getDeadlineByProject(projectId);
+            Hashtable<Integer, String> deadlineDateMapping = deadlineService.getSprintOccurringOnDeadlines(listDeadlines);
             Project project = projectService.getProjectById(projectId);
             Event newEvent = eventService.getNewEvent(project);
             Deadline newDeadline = deadlineService.getNewDeadline(project);
@@ -75,6 +77,7 @@ public class ProjectController {
             model.addAttribute("listDeadlines", listDeadlines);
             model.addAttribute("listSprints", listSprints);
             model.addAttribute("project", project);
+            model.addAttribute("deadlineDateMapping", deadlineDateMapping);
             model.addAttribute("event", newEvent);
             model.addAttribute("deadline", newDeadline);
             model.addAttribute("roles", PrincipalUtils.getUserRole(principal));
