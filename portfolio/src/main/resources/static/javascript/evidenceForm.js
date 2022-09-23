5a6ec8f681ad2775f8d18b59277d7a7f9d2bf82d
Prableen Oberoi <pob16@uclive.ac.nz>
2022-09-24 11:55:26 +1200

    #doc for the evidenceForm.js

--- portfolio/src/main/resources/static/javascript/evidenceForm.js
+++ portfolio/src/main/resources/static/javascript/evidenceForm.js
@@ -3,6 +3,9 @@ let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+/**
+ * Calls the server to save the evidence
+ */
 function saveEvidence() {
     let httpRequest = new XMLHttpRequest();
 
@@ -15,6 +18,9 @@ function saveEvidence() {
     httpRequest.send(formData);
 }
 
+/**
+ * Updates the submission button and current char count displayed to the user.
+ */
 function checkEvidenceTitle(){
     const evidenceTitle = document.getElementById('evidence-title');
     let charMessage = document.getElementById("evidenceCharCount");
@@ -24,6 +30,12 @@ function checkEvidenceTitle(){
 
 }
 
+/**
+ * Updates the modal form based on the data received from the server
+ * @param httpRequest the current http request to the server
+ * @param evidenceProjectId the project ID of the evidence
+ * @param modalTitle modal title based on the selection made
+ */
 function updateEvidenceModalForm(httpRequest, evidenceProjectId, modalTitle) {
 
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
@@ -52,6 +64,9 @@ function updateEvidenceModalForm(httpRequest, evidenceProjectId, modalTitle) {
     }
 }
 
+/**
+ * Server call to create a new evidence
+ */
 function createNewEvidence() {
 
     let httpRequest = new XMLHttpRequest();
@@ -60,6 +75,11 @@ function createNewEvidence() {
     httpRequest.send();
 }
 
+/**
+ * Server to update an existing evidence
+ * @param evidenceId the ID of the evidence selected
+ * @param evidenceProjectId the project ID of the evidence
+ */
 function editEvidence(evidenceId, evidenceProjectId) {
     let httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', `${window.location.pathname}/${evidenceId}/editEvidence`)
@@ -67,6 +87,10 @@ function editEvidence(evidenceId, evidenceProjectId) {
     httpRequest.send();
 
 }
+
+/**
+ * Opens the modal
+ */
 function openEvidenceModal() {
     const modalElement = document.getElementById('evidenceFormModal');
     const modal = bootstrap.Modal.getOrCreateInstance(modalElement, {
@@ -76,12 +100,20 @@ function openEvidenceModal() {
     modal.show();
 }
 
+/**
+ * Checks the evidence description to update the status of the submission button
+ */
 function checkEvidenceDescription() {
     const description = document.getElementById('evidence-description').value;
     document.getElementById('evidenceFormSubmitButton').disabled = description.length === 0;
 
 }
 
+/**
+ * Updates the evidence deletion modal
+ * @param evidenceId the ID of the evidence chosen to be deleted
+ * @param evidenceTitle the title of the evidence chosen to be deleted
+ */
 function updateDeleteDetails(evidenceId, evidenceTitle) {
     document.getElementById('deleteMessage').innerText = `Are you sure you want to delete ${evidenceTitle}?`
     document.getElementById('deleteEvidenceForm').action = `${window.location.pathname}/${evidenceId}/deleteEvidence`
