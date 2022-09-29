6269aad4721bdbbe2517e7848a5cdc80fcd90d07
James Claridge <jcl165@uclive.ac.nz>
2022-09-29 20:38:11 +1300

    Removed a commented out line of code. #refactor.

--- portfolio/src/main/resources/static/javascript/userList.js
+++ portfolio/src/main/resources/static/javascript/userList.js
@@ -17,7 +17,6 @@ function connect() {
     stompClient = new StompJs.Client({
         brokerURL: websocketProtocol + window.location.host + apiPrefix + '/lensfolio-websocket',
         debug: function(str) {
-            // console.log(str);
         },
         reconnectDelay: 5000,
         heartbeatIncoming: 4000,
